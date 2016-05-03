5180c2c5ab6eb59622726368dab10d6099122a45
zhangyh7 <zhangyh7@zhangyh7.ai.com>
2016-05-03 15:34:21 +0800

    订单

---
+++ order-api/src/main/java/com/ai/slp/order/api/demo/interfaces/IDemoSV.java
@@ -0,0 +1,12 @@
+package com.ai.slp.order.api.demo.interfaces;
+
+/**
+ * 
+ * Date: 2016年4月18日 <br>
+ * Copyright (c) 2016 asiainfo.com <br>
+ * 
+ * @author zhaogw
+ */
+public interface IDemoSV {
+
+}
---
+++ order-api/src/main/java/com/ai/slp/order/api/demo/param/DemoParamsRequest.java
@@ -0,0 +1,14 @@
+package com.ai.slp.order.api.demo.param;
+
+import java.io.Serializable;
+
+/**
+ * 请求参数demo.仅用于工程结构说�?,正式环境中请删除<br>
+ *
+ * Date: 2016�?4�?6�? <br>
+ * Copyright (c) 2016 asiainfo.com <br>
+ * @author liutong5
+ */
+public class DemoParamsRequest implements Serializable {
+
+}
