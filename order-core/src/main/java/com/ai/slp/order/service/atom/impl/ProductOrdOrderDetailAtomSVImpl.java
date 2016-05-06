b340fb7982e513f5b51d97155beaadd87bda92e4
zhangyh7 <zhangyh7@zhangyh7.ai.com>
2016-05-06 16:45:46 +0800

    订单详情的接口

---
+++ order-core/src/main/java/com/ai/slp/order/service/atom/impl/ProductOrdOrderDetailAtomSVImpl.java
@@ -0,0 +1,7 @@
+package com.ai.slp.order.service.atom.impl;
+
+import com.ai.slp.order.service.atom.interfaces.IProductOrdOrderDetailAtomSV;
+
+public class ProductOrdOrderDetailAtomSVImpl implements IProductOrdOrderDetailAtomSV{
+    
+}
