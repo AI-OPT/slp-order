01242a36e04a40110755c955b789b98daf051b0d
JackieLiu <liutong6053@163.com>
2016-05-19 16:19:17 +0800

    添加ipass测试

---
+++ order-core/test/java/com/ai/slp/order/dubbo/DubboStartTest.java
@@ -0,0 +1,16 @@
+package com.ai.slp.order.dubbo;
+
+import com.ai.opt.sdk.appserver.DubboServiceStart;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "/context/core-context.xml" })
+public class DubboStartTest {
+    @Test
+    public void testDubboStart(){
+        DubboServiceStart.main(null);
+    }
+}
