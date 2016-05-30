098a23ae44955a611f7696fa6994a8cf9aff8314
jiaxiaoshuo <jiaxs@asiainfo.com>
2016-05-30 16:07:05 +0800

    订单查询

---
+++ order-api/src/main/java/com/ai/slp/order/api/orderlist/param/ProductImage.java
@@ -0,0 +1,48 @@
+package com.ai.slp.order.api.orderlist.param;
+
+import java.io.Serializable;
+
+public class ProductImage implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+     * 上传后的图片ID
+     */
+    private String idpsId;
+
+    /**
+     * 图片扩展名
+     */
+    private String extension;
+
+    /**
+     * 图片尺寸
+     */
+    private String size;
+
+    public String getIdpsId() {
+        return idpsId;
+    }
+
+    public void setIdpsId(String idpsId) {
+        this.idpsId = idpsId;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+}
