5a1c2a08b2f4bbcbd89d9efffacfa1b5e5dd99f0
ccitzxw <changchunme@126.com>
2016-06-14 14:42:37 +0800

    1

--- order-core/src/main/java/com/ai/slp/order/service/business/impl/OrderPayBusiSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/impl/OrderPayBusiSVImpl.java
@@ -316,6 +316,9 @@ public class OrderPayBusiSVImpl implements IOrderPayBusiSV {
             criteria.andOrderIdEqualTo(ordOrder.getOrderId());
         }
         List<OrdOdProdExtend> ordOdProdExtendList = ordOdProdExtendAtomSV.selectByExample(example);
+        if(CollectionUtil.isEmpty(ordOdProdExtendList)){
+            throw new BusinessException(ExceptCodeConstants.Special.PARAM_IS_NULL, "商品明细信息-扩展表信息为空");
+        }
         /* 2.遍历取出值信息 */
         for (OrdOdProdExtend ordOdProdExtend : ordOdProdExtendList) {
             String infoJson = ordOdProdExtend.getInfoJson();
@@ -493,12 +496,12 @@ public class OrderPayBusiSVImpl implements IOrderPayBusiSV {
         routeServReqVo.setUnitPrice(ordOdProdBean.getCostPrice());
         request.setRequestData(JSON.toJSONString(routeServReqVo));
         RouteServerResponse response = iRouteServer.callServerByRouteId(request);
-//        String responseData = response.getResponseData();
-//        if(StringUtil.isBlank(responseData)){
-//            throw new BusinessException(ExceptCodeConstants.Special.PARAM_IS_NULL, "商品明细信息");
-//        }
-//        RouteServResVo routeServResVo = JSON.parseObject(responseData, RouteServResVo.class);
-//        System.out.println(JSON.toJSONString(responseData));
+        String responseData = response.getResponseData();
+        if(StringUtil.isBlank(responseData)){
+            throw new BusinessException(ExceptCodeConstants.Special.PARAM_IS_NULL, "商品明细信息");
+        }
+        RouteServResVo routeServResVo = JSON.parseObject(responseData, RouteServResVo.class);
+        System.out.println(JSON.toJSONString(responseData));
 
     }
 
