83905b49e7eaafa8c2e1d06d21781a72a2d52246
fazhicao <409993207@qq.com>
2016-07-05 17:51:52 +0800

    类上增加注解

--- order-api/src/main/java/com/ai/slp/order/api/ordertradecenter/param/OrdLogisticsInfo.java
+++ order-api/src/main/java/com/ai/slp/order/api/ordertradecenter/param/OrdLogisticsInfo.java
@@ -2,6 +2,8 @@ package com.ai.slp.order.api.ordertradecenter.param;
 
 import java.io.Serializable;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+@JsonIgnoreProperties(ignoreUnknown = true) 
 public class OrdLogisticsInfo implements Serializable {
 
     private static final long serialVersionUID = 1L;
