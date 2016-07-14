7ec7d648cde737612d661096303d3aff765ba18e
fazhicao <409993207@qq.com>
2016-07-14 16:40:15 +0800

    queryApiOrder方法修改返回参数

--- order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IOrdOrderBusiSV.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IOrdOrderBusiSV.java
@@ -3,6 +3,7 @@ package com.ai.slp.order.service.business.interfaces;
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
 import com.ai.slp.order.api.orderlist.param.QueryApiOrderRequest;
+import com.ai.slp.order.api.orderlist.param.QueryApiOrderResponse;
 import com.ai.slp.order.api.orderlist.param.QueryOrderListRequest;
 import com.ai.slp.order.api.orderlist.param.QueryOrderListResponse;
 import com.ai.slp.order.api.orderlist.param.QueryOrderRequest;
@@ -16,6 +17,6 @@ public interface IOrdOrderBusiSV {
     public QueryOrderResponse queryOrder(QueryOrderRequest orderRequest) throws BusinessException,
             SystemException;
 
-    public QueryOrderResponse queryApiOrder(QueryApiOrderRequest orderRequest)
+    public QueryApiOrderResponse queryApiOrder(QueryApiOrderRequest orderRequest)
             throws BusinessException, SystemException;
 }
