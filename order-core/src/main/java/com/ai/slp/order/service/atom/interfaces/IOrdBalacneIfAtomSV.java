8a3482ba16724775e445f6a0c50f44fa1e3b3c68
fazhicao <409993207@qq.com>
2016-07-14 16:37:16 +0800

    添加查询方法

--- order-core/src/main/java/com/ai/slp/order/service/atom/interfaces/IOrdBalacneIfAtomSV.java
+++ order-core/src/main/java/com/ai/slp/order/service/atom/interfaces/IOrdBalacneIfAtomSV.java
@@ -1,7 +1,12 @@
 package com.ai.slp.order.service.atom.interfaces;
 
+import java.util.List;
+
 import com.ai.slp.order.dao.mapper.bo.OrdBalacneIf;
+import com.ai.slp.order.dao.mapper.bo.OrdBalacneIfCriteria;
 
 public interface IOrdBalacneIfAtomSV {
     int insertSelective(OrdBalacneIf record);
+    
+    List<OrdBalacneIf> selectByExample(OrdBalacneIfCriteria example);
 }
