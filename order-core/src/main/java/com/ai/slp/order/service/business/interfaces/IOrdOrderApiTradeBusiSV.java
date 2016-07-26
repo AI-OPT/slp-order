0a1511919e608d7b85bd69b6de91da5d5fef3995
fazhicao <409993207@qq.com>
2016-07-26 12:24:29 +0800

    返回不同实体类

--- order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IOrdOrderApiTradeBusiSV.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IOrdOrderApiTradeBusiSV.java
@@ -2,11 +2,11 @@ package com.ai.slp.order.service.business.interfaces;
 
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
-import com.ai.opt.base.vo.BaseResponse;
 import com.ai.slp.order.api.ordertradecenter.param.OrderApiTradeCenterRequest;
+import com.ai.slp.order.api.ordertradecenter.param.OrderApiTradeCenterResponse;
 
 public interface IOrdOrderApiTradeBusiSV {
 
-    public BaseResponse apiApply(OrderApiTradeCenterRequest request) throws BusinessException,
+    public OrderApiTradeCenterResponse apiApply(OrderApiTradeCenterRequest request) throws BusinessException,
             SystemException;
 }
