88c6c78031a76ca9f031db41fd93026f919c1140
fazhicao <409993207@qq.com>
2016-07-27 15:29:33 +0800

    添加订单轨迹信息

--- order-core/src/main/java/com/ai/slp/order/mds/orderpay/RouteChargeMessProcessorImpl.java
+++ order-core/src/main/java/com/ai/slp/order/mds/orderpay/RouteChargeMessProcessorImpl.java
@@ -17,8 +17,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 import java.sql.Timestamp;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,6 +36,7 @@ public class RouteChargeMessProcessorImpl implements IMessageProcessor {
 
     public RouteChargeMessProcessorImpl(IOrdOrderAtomSV ordOrderAtomSV,IOrderFrameCoreSV orderFrameCoreSV) {
         this.ordOrderAtomSV = ordOrderAtomSV;
+        this.orderFrameCoreSV = orderFrameCoreSV;
     }
 
     @Override
