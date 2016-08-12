e7f8ccff1f5ddcdc8aace0dabb5363298803fd3f
fazhicao <409993207@qq.com>
2016-08-12 13:20:30 +0800

    运费模版

---
+++ order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IFreightTemplateBusiSV.java
@@ -0,0 +1,20 @@
+package com.ai.slp.order.service.business.interfaces;
+
+import com.ai.opt.base.exception.BusinessException;
+import com.ai.opt.base.exception.SystemException;
+import com.ai.slp.order.api.freighttemplate.param.FreightTemplateDeleteRequest;
+import com.ai.slp.order.api.freighttemplate.param.FreightTemplateRequest;
+import com.ai.slp.order.api.freighttemplate.param.FreightTemplateUpdateRequest;
+import com.ai.slp.order.api.freighttemplate.param.QueryFreightTemplateRequest;
+import com.ai.slp.order.api.freighttemplate.param.QueryFreightTemplateResponse;
+
+public interface IFreightTemplateBusiSV {
+	
+	public void add(FreightTemplateRequest request) throws BusinessException, SystemException;
+	
+	public QueryFreightTemplateResponse query(QueryFreightTemplateRequest request) throws BusinessException, SystemException;
+	
+	public void update(FreightTemplateUpdateRequest request) throws BusinessException, SystemException;
+
+	public void delete(FreightTemplateDeleteRequest request); 
+}
