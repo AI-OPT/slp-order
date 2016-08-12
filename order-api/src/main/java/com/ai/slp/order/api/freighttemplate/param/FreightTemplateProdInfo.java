3d009ef98ec05a82892ff708165bfa3b685213c8
fazhicao <409993207@qq.com>
2016-08-12 12:44:58 +0800

    运费参数

---
+++ order-api/src/main/java/com/ai/slp/order/api/freighttemplate/param/FreightTemplateProdInfo.java
@@ -0,0 +1,87 @@
+package com.ai.slp.order.api.freighttemplate.param;
+
+import java.io.Serializable;
+
+public class FreightTemplateProdInfo implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 运送指定地区
+	 */
+	private String transportAddress;
+	
+	/**
+	 * 首次数量
+	 */
+	private Long firstNumber;
+	
+	/**
+	 * 首次金额
+	 */
+	private Long firstNum;
+	
+	/**
+	 * 续数
+	 */
+	private Long pieceNumber;
+	
+	/**
+	 * 续费
+	 */
+	private Long pieceNum;
+	
+	/**
+	 * 对应区域id
+	 */
+	private String regionId;
+	
+	public String getRegionId() {
+		return regionId;
+	}
+
+	public void setRegionId(String regionId) {
+		this.regionId = regionId;
+	}
+
+	public String getTransportAddress() {
+		return transportAddress;
+	}
+
+	public void setTransportAddress(String transportAddress) {
+		this.transportAddress = transportAddress;
+	}
+
+	public Long getFirstNumber() {
+		return firstNumber;
+	}
+
+	public void setFirstNumber(Long firstNumber) {
+		this.firstNumber = firstNumber;
+	}
+
+	public Long getFirstNum() {
+		return firstNum;
+	}
+
+	public void setFirstNum(Long firstNum) {
+		this.firstNum = firstNum;
+	}
+
+	public Long getPieceNumber() {
+		return pieceNumber;
+	}
+
+	public void setPieceNumber(Long pieceNumber) {
+		this.pieceNumber = pieceNumber;
+	}
+
+	public Long getPieceNum() {
+		return pieceNum;
+	}
+
+	public void setPieceNum(Long pieceNum) {
+		this.pieceNum = pieceNum;
+	}
+	
+}
