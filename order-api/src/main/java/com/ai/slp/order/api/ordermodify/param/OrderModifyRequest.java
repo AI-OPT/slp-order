1c36fe722abb1b98f4fe49a5c1e09e92caf423c2
fazhicao <409993207@qq.com>
2016-08-12 13:04:08 +0800

    未支付的订单金额修改服务参数

---
+++ order-api/src/main/java/com/ai/slp/order/api/ordermodify/param/OrderModifyRequest.java
@@ -0,0 +1,55 @@
+package com.ai.slp.order.api.ordermodify.param;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import com.ai.opt.base.vo.BaseInfo;
+
+/**
+ * 未支付订单金额修改参数
+ * @date 2016年8月8日 
+ * @author caofz
+ */
+public class OrderModifyRequest extends BaseInfo{
+
+	private static final long serialVersionUID = 1L;
+	
+	/**
+     * 业务订单ID
+     */
+    private Long orderId;
+    
+    /**
+     * 订单金额
+     */
+    private Long adjustFee;
+    
+    /**
+     * 订单备注
+     */
+    private String remark;
+
+	public Long getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+
+	public Long getAdjustFee() {
+		return adjustFee;
+	}
+
+	public void setAdjustFee(Long adjustFee) {
+		this.adjustFee = adjustFee;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+    
+}
