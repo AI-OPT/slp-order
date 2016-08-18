fccc2b311843ef955c78961c6292ae4420cde369
fazhicao <409993207@qq.com>
2016-08-18 14:32:57 +0800

    添加发货单打印信息

--- order-core/src/main/java/com/ai/slp/order/service/atom/impl/InvoicePrintAtomSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/service/atom/impl/InvoicePrintAtomSVImpl.java
@@ -1,24 +1,18 @@
 package com.ai.slp.order.service.atom.impl;
 
-import java.sql.Timestamp;
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.ai.slp.order.dao.mapper.attach.InvoicePrintAttachMapper;
-import com.ai.slp.order.dao.mapper.attach.OrdOrderProdAttach;
+import com.ai.slp.order.dao.mapper.bo.OrdOdInvoice;
+import com.ai.slp.order.dao.mapper.factory.MapperFactory;
 import com.ai.slp.order.service.atom.interfaces.IInvoicePrintAtomSV;
 @Component
 public class InvoicePrintAtomSVImpl implements IInvoicePrintAtomSV{
-	
-	@Autowired
-	InvoicePrintAttachMapper invoicePrintAttachMapper;
 
 	@Override
-	public List<OrdOrderProdAttach> query(String userId, String tenantId, String skuId, String routeId, String state,
-			Timestamp timeBefore, Timestamp timeAfter) {
-		return invoicePrintAttachMapper.query(userId, tenantId,skuId, routeId, state,timeBefore,timeAfter);
+	public int insertSelective(OrdOdInvoice record) {
+		return MapperFactory.getOrdOdInvoiceMapper().insertSelective(record);
 	}
+	
 
 }
