cd90479b4f6385d99cb6117da0e26e361c384149
fazhicao <409993207@qq.com>
2016-08-21 11:04:02 +0800

    添加服务

--- order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IDeliveryOrderPrintBusiSV.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IDeliveryOrderPrintBusiSV.java
@@ -4,11 +4,14 @@ import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
 import com.ai.slp.order.api.deliveryorderprint.param.DeliveryOrderPrintRequest;
 import com.ai.slp.order.api.deliveryorderprint.param.DeliveryOrderPrintResponse;
+import com.ai.slp.order.api.deliveryorderprint.param.DeliveryOrderQueryResponse;
 
 public interface IDeliveryOrderPrintBusiSV {
 	
-	public void print(DeliveryOrderPrintRequest request) throws BusinessException, SystemException;
+	public DeliveryOrderQueryResponse query(DeliveryOrderPrintRequest request);
 
-	public DeliveryOrderPrintResponse query(DeliveryOrderPrintRequest request);
+	public DeliveryOrderPrintResponse display(DeliveryOrderPrintRequest request);
+
+	public void print(DeliveryOrderPrintRequest request) throws BusinessException, SystemException;
 
 }
