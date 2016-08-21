6d367a7379b9287bf3362efc71be7d90e0e4012e
fazhicao <409993207@qq.com>
2016-08-21 23:43:20 +0800

    参数的修改

--- order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IDeliveryOrderPrintBusiSV.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IDeliveryOrderPrintBusiSV.java
@@ -2,6 +2,7 @@ package com.ai.slp.order.service.business.interfaces;
 
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
+import com.ai.slp.order.api.deliveryorderprint.param.DeliveryOrderPrintInfosRequest;
 import com.ai.slp.order.api.deliveryorderprint.param.DeliveryOrderPrintRequest;
 import com.ai.slp.order.api.deliveryorderprint.param.DeliveryOrderPrintResponse;
 import com.ai.slp.order.api.deliveryorderprint.param.DeliveryOrderQueryResponse;
@@ -12,6 +13,6 @@ public interface IDeliveryOrderPrintBusiSV {
 
 	public DeliveryOrderPrintResponse display(DeliveryOrderPrintRequest request);
 
-	public void print(DeliveryOrderPrintRequest request) throws BusinessException, SystemException;
+	public void print(DeliveryOrderPrintInfosRequest request) throws BusinessException, SystemException;
 
 }
