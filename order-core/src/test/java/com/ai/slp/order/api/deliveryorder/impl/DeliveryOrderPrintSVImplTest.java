e20ae475e649e072b376f4a31c4c2f550b29e238
liutong <liutong6053@163.com>
2016-08-22 09:25:03 +0800

    完善单元测试.

--- order-core/src/test/java/com/ai/slp/order/api/deliveryorder/impl/DeliveryOrderPrintSVImplTest.java
+++ order-core/src/test/java/com/ai/slp/order/api/deliveryorder/impl/DeliveryOrderPrintSVImplTest.java
@@ -1,14 +1,13 @@
 package com.ai.slp.order.api.deliveryorder.impl;
 
+import com.ai.opt.base.vo.BaseResponse;
+import com.ai.slp.order.api.deliveryorderprint.interfaces.IDeliveryOrderPrintSV;
+import com.ai.slp.order.api.deliveryorderprint.param.DeliveryOrderPrintRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.ai.slp.order.api.deliveryorderprint.interfaces.IDeliveryOrderPrintSV;
-import com.ai.slp.order.api.deliveryorderprint.param.DeliveryOrderPrintRequest;
-import com.ai.slp.order.api.deliveryorderprint.param.DeliveryOrderPrintResponse;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "/context/core-context.xml" })
@@ -22,7 +21,7 @@ public class DeliveryOrderPrintSVImplTest {
 		request.setOrderId(35913355l);
 		request.setUserId("000000000000000945");
 		request.setTenantId("SLP");
-		DeliveryOrderPrintResponse response = deliveryOrderPrintSV.print(request);
+		BaseResponse response = deliveryOrderPrintSV.print(request);
 		System.out.println(response);
 	}
 
