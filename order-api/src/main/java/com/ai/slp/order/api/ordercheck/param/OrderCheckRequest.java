4339eb38cc662cd1a10d6a60fd94126d5643c6bc
fazhicao <409993207@qq.com>
2016-08-24 17:31:37 +0800

    审核字段修改

--- order-api/src/main/java/com/ai/slp/order/api/ordercheck/param/OrderCheckRequest.java
+++ order-api/src/main/java/com/ai/slp/order/api/ordercheck/param/OrderCheckRequest.java
@@ -18,6 +18,21 @@ public class OrderCheckRequest extends BaseInfo{
 	 */
 	@NotNull(message = "订单ID不能为空")
 	private long orderId;
+	
+	/**
+	 * 审核结果
+	 */
+	private int checkResult; //1 同意,0 拒绝
+	
+	/**
+	 * 审核结果描述
+	 */
+	private String remark;
+	
+	/**
+	 * 审核人
+	 */
+	private String operId;
 
 	public long getOrderId() {
 		return orderId;
@@ -25,6 +40,30 @@ public class OrderCheckRequest extends BaseInfo{
 
 	public void setOrderId(long orderId) {
 		this.orderId = orderId;
+	}
+
+	public int getCheckResult() {
+		return checkResult;
+	}
+
+	public void setCheckResult(int checkResult) {
+		this.checkResult = checkResult;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	public String getOperId() {
+		return operId;
+	}
+
+	public void setOperId(String operId) {
+		this.operId = operId;
 	}
 	
 }
