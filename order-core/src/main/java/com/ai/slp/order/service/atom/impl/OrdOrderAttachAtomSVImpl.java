4989ba8564caf427a0c123617e4039a74fdbd101
fazhicao <409993207@qq.com>
2016-08-25 15:30:29 +0800

    运营后台订单列表查询原子层功能实现

--- order-core/src/main/java/com/ai/slp/order/service/atom/impl/OrdOrderAttachAtomSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/service/atom/impl/OrdOrderAttachAtomSVImpl.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.ai.slp.order.api.orderlist.param.BehindQueryOrderListRequest;
 import com.ai.slp.order.api.orderlist.param.QueryOrderListRequest;
+import com.ai.slp.order.dao.mapper.attach.BehindOrdOrderAttach;
 import com.ai.slp.order.dao.mapper.attach.OrdOrderAttach;
 import com.ai.slp.order.dao.mapper.attach.OrdOrderAttachMapper;
 import com.ai.slp.order.service.atom.interfaces.IOrdOrderAttachAtomSV;
@@ -34,4 +36,21 @@ public class OrdOrderAttachAtomSVImpl implements IOrdOrderAttachAtomSV {
                 orderListRequest.getPayStyle(), orderListRequest.getTenantId(), states,
                 orderListRequest.getOrderTimeBegin(), orderListRequest.getOrderTimeEnd());
     }
+
+	@Override
+	public int behindQueryCount(BehindQueryOrderListRequest orderListRequest, String states) {
+		 return ordOrderAttachMapper.getBehindCount( orderListRequest.getChlId(),
+				 orderListRequest.getDeliveryFlag(), orderListRequest.getOrderId(),
+	                orderListRequest.getTenantId(), states,
+	                orderListRequest.getOrderTimeBegin(), orderListRequest.getOrderTimeEnd());
+	}
+
+	@Override
+	public List<BehindOrdOrderAttach> behindQueryOrderBySearch(BehindQueryOrderListRequest orderListRequest, String states) {
+		return ordOrderAttachMapper.getBehindOrdOrder((orderListRequest.getPageNo() - 1)* orderListRequest.getPageSize(), 
+				orderListRequest.getPageSize(),
+                orderListRequest.getChlId(),orderListRequest.getDeliveryFlag(),
+                orderListRequest.getOrderId(),orderListRequest.getTenantId(), 
+                states, orderListRequest.getOrderTimeBegin(),orderListRequest.getOrderTimeEnd());
+	}
 }
