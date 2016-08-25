946fce5566836f597e3595f471311d004cdc67db
fazhicao <409993207@qq.com>
2016-08-25 15:29:59 +0800

    运营后台订单列表查询语句

--- order-core/src/main/java/com/ai/slp/order/dao/mapper/attach/OrdOrderSqlProvider.java
+++ order-core/src/main/java/com/ai/slp/order/dao/mapper/attach/OrdOrderSqlProvider.java
@@ -68,4 +68,67 @@ public class OrdOrderSqlProvider {
         seqBuffer.append(" and oo.order_id=of.order_id");
         return seqBuffer.toString();
     }
+    
+    
+    
+    /**
+     * 查询我的订单信息
+     * 
+     * @param param
+     * @return
+     * @author caofz
+     */
+    public String behindQueryOrdOrder(Map<String, Object> param) {
+        StringBuffer seqBuffer = new StringBuffer();
+        seqBuffer.append("select oo.order_id,oo.tenant_id,oo.chl_id,state,oo.delivery_flag,"
+                + "oo.parent_order_id,oo.user_id,"
+                + "discount_fee,adjust_fee from "
+                + "ord_order oo,ord_od_fee_total of where"
+                + " oo.tenant_id= '"+ param.get("tenantId") + "'");
+        String chlId = param.containsKey("chlId") ? (String) param.get("chlId") : null;
+        if (!StringUtil.isBlank(chlId))
+            seqBuffer.append(" and oo.chl_id = " + chlId);
+        if (param.get("orderId") != null)
+            seqBuffer.append(" and oo.order_id = " + param.get("orderId"));
+        String deliveryFlag = param.containsKey("deliveryFlag") ? (String) param.get("deliveryFlag") : null;
+        if (!StringUtil.isBlank(deliveryFlag))
+            seqBuffer.append(" and oo.delivery_flag=" + deliveryFlag);
+        String states = param.containsKey("states") ? (String) param.get("states") : null;
+        if (!StringUtil.isBlank(states))
+            seqBuffer.append(" and oo.state in(" + states + ")");
+        if (param.get("orderTimeBegin") != null && param.get("orderTimeEnd") != null) {
+            seqBuffer.append(" and oo.order_time between '" + param.get("orderTimeBegin")
+                    + "' and '" + param.get("orderTimeEnd") + "'");
+        }
+        seqBuffer.append(" and oo.order_id=of.order_id order by order_time desc limit "
+                + param.get("pageCount") + "," + param.get("pageSize"));
+        return seqBuffer.toString();
+    }
+
+    /**
+     * 多表查询订单个数
+     */
+    public String behindCount(Map<String, Object> param) {
+        StringBuffer seqBuffer = new StringBuffer();
+        seqBuffer.append("select count(*) from "
+                + "ord_order oo,ord_od_fee_total of where"
+                + " oo.TENANT_ID= '"+ param.get("tenantId") + "'");
+        String chlId = param.containsKey("chlId") ? (String) param.get("chlId") : null;
+        if (!StringUtil.isBlank(chlId))
+            seqBuffer.append(" and oo.chl_id = " + chlId);
+        if (param.get("orderId") != null)
+            seqBuffer.append(" and oo.order_id = " + param.get("orderId"));
+        String deliveryFlag = param.containsKey("deliveryFlag") ? (String) param.get("deliveryFlag") : null;
+        if (!StringUtil.isBlank(deliveryFlag))
+            seqBuffer.append(" and oo.delivery_flag=" + deliveryFlag);
+        String states = param.containsKey("states") ? (String) param.get("states") : null;
+        if (!StringUtil.isBlank(states))
+            seqBuffer.append(" and oo.state in(" + states + ")");
+        if (param.get("orderTimeBegin") != null && param.get("orderTimeEnd") != null) {
+            seqBuffer.append(" and oo.order_time between '" + param.get("orderTimeBegin")
+                    + "' and '" + param.get("orderTimeEnd") + "'");
+        }
+        seqBuffer.append(" and oo.order_id=of.order_id");
+        return seqBuffer.toString();
+    }
 }
