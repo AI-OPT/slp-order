33ae3c7bef524628a562b4dfc19f15fe630891b9
fazhicao <409993207@qq.com>
2016-08-25 15:25:28 +0800

    运营后台订单列表返回的报文体

---
+++ order-api/src/main/java/com/ai/slp/order/api/orderlist/param/BehindQueryOrderListResponse.java
@@ -0,0 +1,23 @@
+package com.ai.slp.order.api.orderlist.param;
+
+import com.ai.opt.base.vo.BaseResponse;
+import com.ai.opt.base.vo.PageInfo;
+
+public class BehindQueryOrderListResponse extends BaseResponse{
+
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * 订单列表信息
+	 */
+	 private PageInfo<BehindOrdOrderVo> pageInfo;
+
+	public PageInfo<BehindOrdOrderVo> getPageInfo() {
+		return pageInfo;
+	}
+
+	public void setPageInfo(PageInfo<BehindOrdOrderVo> pageInfo) {
+		this.pageInfo = pageInfo;
+	}
+
+}
