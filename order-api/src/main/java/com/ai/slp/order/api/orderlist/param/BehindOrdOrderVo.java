92e9542b21041ba96734c756ed04305f2b3f22c3
fazhicao <409993207@qq.com>
2016-08-27 14:11:16 +0800

    添加积分

--- order-api/src/main/java/com/ai/slp/order/api/orderlist/param/BehindOrdOrderVo.java
+++ order-api/src/main/java/com/ai/slp/order/api/orderlist/param/BehindOrdOrderVo.java
@@ -1,6 +1,5 @@
 package com.ai.slp.order.api.orderlist.param;
 
-import java.sql.Timestamp;
 import java.util.List;
 
 import com.ai.opt.base.vo.BaseInfo;
@@ -16,53 +15,30 @@ public class BehindOrdOrderVo extends BaseInfo {
     private static final long serialVersionUID = 1L;
     
     /**
-     * 订单来源  受理渠道
+     * 订单来源 (受理渠道)
      */
     private String chlId;
     
     /**
      * 父订单id
      */
-    private Long parentOrderId;
+    private Long pOrderId;
     
     /**
-     * 买家帐号      userid
+     * 买家帐号 (userid)
      */
     private String userId;
     
     /**
-     * 绑定手机号 ??
+     * 绑定手机号 (用户相关)
      */
-    
+    private String userTel;
     
     /**
      * 是否需要物流
      */
     private String deliveryFlag;
     
-    /**
-     * 业务订单ID
-     */
-    private Long orderId;
-    
-
-    /**
-     * 订单状态(后厂)
-     */
-    private String state;
-
-    /**
-     * 订单状态展示
-     */
-    private String stateName;
-
-
-    /**
-     * 下单时间
-     */
-    private Timestamp orderTime;
-
-
     /**
      * 总优惠金额
      */
@@ -80,12 +56,11 @@ public class BehindOrdOrderVo extends BaseInfo {
      */
     private String contactTel;
     
-
     /**
-     * 支付信息
+     * 积分
      */
-    private List<OrderPayVo> payDataList;
-
+    private Long points;
+    
     /**
      * 商品集合
      */
@@ -99,12 +74,20 @@ public class BehindOrdOrderVo extends BaseInfo {
 		this.chlId = chlId;
 	}
 
-	public Long getParentOrderId() {
-		return parentOrderId;
+	public Long getpOrderId() {
+		return pOrderId;
 	}
 
-	public void setParentOrderId(Long parentOrderId) {
-		this.parentOrderId = parentOrderId;
+	public void setpOrderId(Long pOrderId) {
+		this.pOrderId = pOrderId;
+	}
+
+	public String getUserTel() {
+		return userTel;
+	}
+
+	public void setUserTel(String userTel) {
+		this.userTel = userTel;
 	}
 
 	public String getUserId() {
@@ -121,38 +104,6 @@ public class BehindOrdOrderVo extends BaseInfo {
 
 	public void setDeliveryFlag(String deliveryFlag) {
 		this.deliveryFlag = deliveryFlag;
-	}
-
-	public Long getOrderId() {
-		return orderId;
-	}
-
-	public void setOrderId(Long orderId) {
-		this.orderId = orderId;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getStateName() {
-		return stateName;
-	}
-
-	public void setStateName(String stateName) {
-		this.stateName = stateName;
-	}
-
-	public Timestamp getOrderTime() {
-		return orderTime;
-	}
-
-	public void setOrderTime(Timestamp orderTime) {
-		this.orderTime = orderTime;
 	}
 
 	public Long getDiscountFee() {
@@ -178,13 +129,13 @@ public class BehindOrdOrderVo extends BaseInfo {
 	public void setContactTel(String contactTel) {
 		this.contactTel = contactTel;
 	}
-
-	public List<OrderPayVo> getPayDataList() {
-		return payDataList;
+	
+	public Long getPoints() {
+		return points;
 	}
 
-	public void setPayDataList(List<OrderPayVo> payDataList) {
-		this.payDataList = payDataList;
+	public void setPoints(Long points) {
+		this.points = points;
 	}
 
 	public List<BehindOrdProductVo> getProductList() {
@@ -194,5 +145,4 @@ public class BehindOrdOrderVo extends BaseInfo {
 	public void setProductList(List<BehindOrdProductVo> productList) {
 		this.productList = productList;
 	}
-
 }
