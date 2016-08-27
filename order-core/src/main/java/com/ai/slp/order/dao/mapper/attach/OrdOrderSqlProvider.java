62986dd4ac83d0b09dbcf43a022e658db93ec02c
fazhicao <409993207@qq.com>
2016-08-27 14:14:05 +0800

    多表多条件下的sql语句

--- order-core/src/main/java/com/ai/slp/order/dao/mapper/attach/OrdOrderSqlProvider.java
+++ order-core/src/main/java/com/ai/slp/order/dao/mapper/attach/OrdOrderSqlProvider.java
@@ -55,6 +55,9 @@ public class OrdOrderSqlProvider {
             seqBuffer.append(" and oo.order_type = " + orderType);
         if (param.get("orderId") != null)
             seqBuffer.append(" and oo.order_id = " + param.get("orderId"));
+        String userId = param.containsKey("userId") ? (String) param.get("userId") : null;
+        if (param.get("userId") != null)
+            seqBuffer.append(" and oo.user_id = " +userId);
         String payStyle = param.containsKey("payStyle") ? (String) param.get("payStyle") : null;
         if (!StringUtil.isBlank(payStyle))
             seqBuffer.append(" and of.pay_style=" + payStyle);
@@ -72,7 +75,7 @@ public class OrdOrderSqlProvider {
     
     
     /**
-     * 查询我的订单信息
+     * 运营后台订单列表查询信息
      * 
      * @param param
      * @return
@@ -80,19 +83,23 @@ public class OrdOrderSqlProvider {
      */
     public String behindQueryOrdOrder(Map<String, Object> param) {
         StringBuffer seqBuffer = new StringBuffer();
-        seqBuffer.append("select oo.order_id,oo.tenant_id,oo.chl_id,state,oo.delivery_flag,"
-                + "oo.parent_order_id,oo.user_id,"
-                + "discount_fee,adjust_fee from "
-                + "ord_order oo,ord_od_fee_total of where"
+        seqBuffer.append("select oo.order_id,oo.chl_id,oo.delivery_flag,"
+        		+ "contact_tel,oo.user_id,discount_fee,adjust_fee from ord_order oo,ord_od_logistics ol,ord_od_fee_total of where"
                 + " oo.tenant_id= '"+ param.get("tenantId") + "'");
         String chlId = param.containsKey("chlId") ? (String) param.get("chlId") : null;
         if (!StringUtil.isBlank(chlId))
             seqBuffer.append(" and oo.chl_id = " + chlId);
         if (param.get("orderId") != null)
-            seqBuffer.append(" and oo.order_id = " + param.get("orderId"));
+            seqBuffer.append(" and oo.order_id =" +param.get("orderId"));
+        String userId = param.containsKey("userId") ? (String) param.get("userId") : null;
+        if (param.get("userId") != null)
+            seqBuffer.append(" and oo.user_id = " +userId);
         String deliveryFlag = param.containsKey("deliveryFlag") ? (String) param.get("deliveryFlag") : null;
         if (!StringUtil.isBlank(deliveryFlag))
             seqBuffer.append(" and oo.delivery_flag=" + deliveryFlag);
+        String contactTel = param.containsKey("contactTel") ? (String) param.get("contactTel") : null;
+        if (!StringUtil.isBlank(contactTel))
+        	seqBuffer.append(" and ol.contactTel like" + "%"+contactTel+"%");
         String states = param.containsKey("states") ? (String) param.get("states") : null;
         if (!StringUtil.isBlank(states))
             seqBuffer.append(" and oo.state in(" + states + ")");
@@ -100,7 +107,8 @@ public class OrdOrderSqlProvider {
             seqBuffer.append(" and oo.order_time between '" + param.get("orderTimeBegin")
                     + "' and '" + param.get("orderTimeEnd") + "'");
         }
-        seqBuffer.append(" and oo.order_id=of.order_id order by order_time desc limit "
+        seqBuffer.append(" and oo.order_id=ol.order_id and oo.order_id=of.order_id "
+        		+ "and of.order_id=ol.order_id order by order_time desc limit "
                 + param.get("pageCount") + "," + param.get("pageSize"));
         return seqBuffer.toString();
     }
@@ -110,17 +118,19 @@ public class OrdOrderSqlProvider {
      */
     public String behindCount(Map<String, Object> param) {
         StringBuffer seqBuffer = new StringBuffer();
-        seqBuffer.append("select count(*) from "
-                + "ord_order oo,ord_od_fee_total of where"
-                + " oo.TENANT_ID= '"+ param.get("tenantId") + "'");
+        seqBuffer.append("select count(*) from ord_order oo,ord_od_logistics ol,ord_od_fee_total of where"
+                + " oo.tenant_id= '"+ param.get("tenantId") + "'");
         String chlId = param.containsKey("chlId") ? (String) param.get("chlId") : null;
         if (!StringUtil.isBlank(chlId))
             seqBuffer.append(" and oo.chl_id = " + chlId);
         if (param.get("orderId") != null)
-            seqBuffer.append(" and oo.order_id = " + param.get("orderId"));
+        	seqBuffer.append(" and oo.order_id =" + param.get("orderId"));
         String deliveryFlag = param.containsKey("deliveryFlag") ? (String) param.get("deliveryFlag") : null;
         if (!StringUtil.isBlank(deliveryFlag))
             seqBuffer.append(" and oo.delivery_flag=" + deliveryFlag);
+        String contactTel = param.containsKey("contactTel") ? (String) param.get("contactTel") : null;
+        if (!StringUtil.isBlank(contactTel))
+        	seqBuffer.append(" and ol.contactTel like" + "%"+contactTel+"%");
         String states = param.containsKey("states") ? (String) param.get("states") : null;
         if (!StringUtil.isBlank(states))
             seqBuffer.append(" and oo.state in(" + states + ")");
@@ -128,7 +138,7 @@ public class OrdOrderSqlProvider {
             seqBuffer.append(" and oo.order_time between '" + param.get("orderTimeBegin")
                     + "' and '" + param.get("orderTimeEnd") + "'");
         }
-        seqBuffer.append(" and oo.order_id=of.order_id");
+        seqBuffer.append(" and oo.order_id=ol.order_id and oo.order_id=of.order_id and of.order_id=ol.order_id");
         return seqBuffer.toString();
     }
 }
