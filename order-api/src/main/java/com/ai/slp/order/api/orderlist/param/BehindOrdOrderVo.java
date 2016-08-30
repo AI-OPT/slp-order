516428c830bb8f9b0ce496287af0541f420c923f
fazhicao <409993207@qq.com>
2016-08-30 10:03:35 +0800

    订单查询列表返回信息

--- order-api/src/main/java/com/ai/slp/order/api/orderlist/param/BehindOrdOrderVo.java
+++ order-api/src/main/java/com/ai/slp/order/api/orderlist/param/BehindOrdOrderVo.java
@@ -4,138 +4,51 @@ import java.util.List;
 
 import com.ai.opt.base.vo.BaseInfo;
 
-/**
- * 订单相关信息 Date: 2016年8月25日 <br>
- * Copyright (c) 2016 asiainfo.com <br>
- * 
- * @author caofz
- */
-public class BehindOrdOrderVo extends BaseInfo {
+public class BehindOrdOrderVo extends BaseInfo{
 
-    private static final long serialVersionUID = 1L;
-    
+	private static final long serialVersionUID = 1L;
+	
     /**
-     * 订单来源 (受理渠道)
+     * 业务订单ID
      */
-    private String chlId;
+    private Long orderId;
     
-    /**
-     * 父订单id
-     */
-    private Long pOrderId;
-    
-    /**
-     * 买家帐号 (userid)
-     */
-    private String userId;
-    
-    /**
-     * 绑定手机号 (用户相关)
-     */
-    private String userTel;
-    
-    /**
-     * 是否需要物流
-     */
-    private String deliveryFlag;
-    
-    /**
-     * 总优惠金额
-     */
-    private Long discountFee;
-
 
     /**
-     * 总实收费用
+     * 订单状态(后厂)
      */
-    private Long adjustFee;
+    private String state;
 
-    
     /**
-     * 收件人电话
+     * 订单状态展示
      */
-    private String contactTel;
+    private String stateName;
     
-    /**
-     * 积分
-     */
-    private Long points;
     
-    /**
-     * 商品集合
-     */
     private List<BehindOrdProductVo> productList;
-
-	public String getChlId() {
-		return chlId;
+    
+	public Long getOrderId() {
+		return orderId;
 	}
 
-	public void setChlId(String chlId) {
-		this.chlId = chlId;
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
 	}
 
-	public Long getpOrderId() {
-		return pOrderId;
+	public String getState() {
+		return state;
 	}
 
-	public void setpOrderId(Long pOrderId) {
-		this.pOrderId = pOrderId;
+	public void setState(String state) {
+		this.state = state;
 	}
 
-	public String getUserTel() {
-		return userTel;
+	public String getStateName() {
+		return stateName;
 	}
 
-	public void setUserTel(String userTel) {
-		this.userTel = userTel;
-	}
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	public String getDeliveryFlag() {
-		return deliveryFlag;
-	}
-
-	public void setDeliveryFlag(String deliveryFlag) {
-		this.deliveryFlag = deliveryFlag;
-	}
-
-	public Long getDiscountFee() {
-		return discountFee;
-	}
-
-	public void setDiscountFee(Long discountFee) {
-		this.discountFee = discountFee;
-	}
-	
-	public Long getAdjustFee() {
-		return adjustFee;
-	}
-
-	public void setAdjustFee(Long adjustFee) {
-		this.adjustFee = adjustFee;
-	}
-
-	public String getContactTel() {
-		return contactTel;
-	}
-
-	public void setContactTel(String contactTel) {
-		this.contactTel = contactTel;
-	}
-	
-	public Long getPoints() {
-		return points;
-	}
-
-	public void setPoints(Long points) {
-		this.points = points;
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
 	}
 
 	public List<BehindOrdProductVo> getProductList() {
