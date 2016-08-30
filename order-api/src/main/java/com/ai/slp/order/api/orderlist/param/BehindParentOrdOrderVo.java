25ef02cd4c4dee850344f4913a3a43267e797518
fazhicao <409993207@qq.com>
2016-08-30 10:04:09 +0800

    订单列表查询返回信息

---
+++ order-api/src/main/java/com/ai/slp/order/api/orderlist/param/BehindParentOrdOrderVo.java
@@ -0,0 +1,148 @@
+package com.ai.slp.order.api.orderlist.param;
+
+import java.util.List;
+
+import com.ai.opt.base.vo.BaseInfo;
+
+/**
+ * 订单相关信息 Date: 2016年8月25日 <br>
+ * Copyright (c) 2016 asiainfo.com <br>
+ * 
+ * @author caofz
+ */
+public class BehindParentOrdOrderVo extends BaseInfo {
+
+    private static final long serialVersionUID = 1L;
+    
+    /**
+     * 订单来源 (受理渠道)
+     */
+    private String chlId;
+    
+    /**
+     * 父订单id
+     */
+    private Long pOrderId;
+    
+    /**
+     * 买家帐号 (userid)
+     */
+    private String userId;
+    
+    /**
+     * 绑定手机号 (用户相关)
+     */
+    private String userTel;
+    
+    /**
+     * 是否需要物流
+     */
+    private String deliveryFlag;
+    
+    /**
+     * 总优惠金额
+     */
+    private Long discountFee;
+
+
+    /**
+     * 总实收费用
+     */
+    private Long adjustFee;
+
+    
+    /**
+     * 收件人电话
+     */
+    private String contactTel;
+    
+    /**
+     * 积分
+     */
+    private Long points;
+    
+    /**
+     * 订单及商品信息
+     */
+    private List<BehindOrdOrderVo> orderList;
+
+	public String getChlId() {
+		return chlId;
+	}
+
+	public void setChlId(String chlId) {
+		this.chlId = chlId;
+	}
+
+	public Long getpOrderId() {
+		return pOrderId;
+	}
+
+	public void setpOrderId(Long pOrderId) {
+		this.pOrderId = pOrderId;
+	}
+
+	public String getUserTel() {
+		return userTel;
+	}
+
+	public void setUserTel(String userTel) {
+		this.userTel = userTel;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getDeliveryFlag() {
+		return deliveryFlag;
+	}
+
+	public void setDeliveryFlag(String deliveryFlag) {
+		this.deliveryFlag = deliveryFlag;
+	}
+
+	public Long getDiscountFee() {
+		return discountFee;
+	}
+
+	public void setDiscountFee(Long discountFee) {
+		this.discountFee = discountFee;
+	}
+	
+	public Long getAdjustFee() {
+		return adjustFee;
+	}
+
+	public void setAdjustFee(Long adjustFee) {
+		this.adjustFee = adjustFee;
+	}
+
+	public String getContactTel() {
+		return contactTel;
+	}
+
+	public void setContactTel(String contactTel) {
+		this.contactTel = contactTel;
+	}
+	
+	public Long getPoints() {
+		return points;
+	}
+
+	public void setPoints(Long points) {
+		this.points = points;
+	}
+
+	public List<BehindOrdOrderVo> getOrderList() {
+		return orderList;
+	}
+
+	public void setOrderList(List<BehindOrdOrderVo> orderList) {
+		this.orderList = orderList;
+	}
+}
