0bc61412cc1e9847f904a5944782390d84cd3cd7
fazhicao <409993207@qq.com>
2016-09-05 22:50:13 +0800

    未支付订单金额修改的测试类

--- order-core/src/test/java/com/ai/slp/order/api/ordermodify/impl/NotPaidOrderModifySVImplTest.java
+++ order-core/src/test/java/com/ai/slp/order/api/ordermodify/impl/NotPaidOrderModifySVImplTest.java
@@ -1,6 +1,7 @@
 package com.ai.slp.order.api.ordermodify.impl;
 
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,13 +21,17 @@ public class NotPaidOrderModifySVImplTest {
 
 	@Test
 	public void test() {
-		OrderModifyRequest request=new OrderModifyRequest();
-		request.setOrderId(2456229l);
-		request.setAdjustFee(0l);
-		request.setTenantId("SLP");
-		request.setRemark("121212");
+        OrderModifyRequest request=new OrderModifyRequest();
+        request.setOrderId(3345703912l);
+		request.setUpdateAmount(4000l);
+		request.setTenantId("changhong");
+		//request.setUpdateRemark("121212");
 		BaseResponse response = notPaidOrderModifySV.modify(request);
 		System.out.println(response);
 	}
+	
+	  public static String trim(String str) {  
+	        return ((str == null) ? "" : str.trim());  
+	    } 
 
 }
