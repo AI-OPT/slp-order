8c26fd0414977c50f380a56e4bd0262b6a78e282
fazhicao <409993207@qq.com>
2016-09-08 09:36:05 +0800

    入参修改

--- order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IOrderRefundBusiSV.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IOrderRefundBusiSV.java
@@ -2,16 +2,14 @@ package com.ai.slp.order.service.business.interfaces;
 
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
-import com.ai.opt.base.vo.BaseResponse;
-import com.ai.slp.order.api.orderrefund.param.OrderFullRefundRequest;
-import com.ai.slp.order.api.orderrefund.param.OrderPartRefundRequest;
+import com.ai.slp.order.api.orderrefund.param.OrderRefundRequest;
 import com.ai.slp.order.api.orderrefund.param.OrderRefuseRefundRequest;
 
 public interface IOrderRefundBusiSV {
 	
-	public BaseResponse fullRefund(OrderFullRefundRequest request) throws BusinessException, SystemException;
+	public void fullRefund(OrderRefundRequest request) throws BusinessException, SystemException;
 
-	public BaseResponse partRefund(OrderPartRefundRequest request) throws BusinessException, SystemException;
+	public void partRefund(OrderRefundRequest request) throws BusinessException, SystemException;
 	
-	public BaseResponse refuseRefund(OrderRefuseRefundRequest request) throws BusinessException, SystemException;
+	public void refuseRefund(OrderRefuseRefundRequest request) throws BusinessException, SystemException;
 }
