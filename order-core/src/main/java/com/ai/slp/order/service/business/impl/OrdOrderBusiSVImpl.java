0b39a10bbd7a06f5317a32177be631cbf471fb0d
fazhicao <409993207@qq.com>
2016-09-09 16:21:15 +0800

    售后状态修改

--- order-core/src/main/java/com/ai/slp/order/service/business/impl/OrdOrderBusiSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/impl/OrdOrderBusiSVImpl.java
@@ -681,8 +681,8 @@ public class OrdOrderBusiSVImpl implements IOrdOrderBusiSV {
         		SysParam sysParamDf = this.translateInfo(behindOrdOrderAttach.getTenantId(), 
         				"ORD_ORDER", "ORD_DELIVERY_FLAG", behindOrdOrderAttach.getDeliveryFlag(), iCacheSV);
         		pOrderVo.setDeliveryFlagName(sysParamDf==null?"":sysParamDf.getColumnDesc());
-        		String[] arr={"21","22","23","31","92","93","94"};  //售后状态
-        		boolean flag = Arrays.asList(arr).contains(behindOrdOrderAttach.getState());
+        		String arr="21,22,23,31,92,93,94";  //售后状态
+        		boolean flag=arr.equals(states);
         		if(!flag) {
         			pOrderVo.setAdjustFee(behindOrdOrderAttach.getAdjustFee());
         		}
