1ed7e2e4f6ba6bfc52391dc414d475c139ecb7f9
fazhicao <409993207@qq.com>
2016-09-12 09:53:25 +0800

    订单发货服务实现类

---
+++ order-core/src/main/java/com/ai/slp/order/api/delivergoods/impl/DeliverGoodsSVImpl.java
@@ -0,0 +1,33 @@
+package com.ai.slp.order.api.delivergoods.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.ai.opt.base.exception.BusinessException;
+import com.ai.opt.base.exception.SystemException;
+import com.ai.opt.base.vo.BaseResponse;
+import com.ai.opt.base.vo.ResponseHeader;
+import com.ai.opt.sdk.constants.ExceptCodeConstants;
+import com.ai.slp.order.api.delivergoods.interfaces.IDeliverGoodsSV;
+import com.ai.slp.order.api.delivergoods.param.DeliverGoodsRequest;
+import com.ai.slp.order.service.business.interfaces.IDeliverGoodsBusiSV;
+import com.alibaba.dubbo.config.annotation.Service;
+
+@Service(validation="true")
+@Component
+public class DeliverGoodsSVImpl implements IDeliverGoodsSV {
+	
+	@Autowired
+	private IDeliverGoodsBusiSV deliverGoodsBusiSV;
+	
+	@Override
+	public BaseResponse deliverGoods(DeliverGoodsRequest request) throws BusinessException, SystemException {
+		BaseResponse response=new BaseResponse();
+		deliverGoodsBusiSV.deliverGoods(request);
+		ResponseHeader responseHeader = new ResponseHeader(true,
+	                ExceptCodeConstants.Special.SUCCESS, "成功");
+	    response.setResponseHeader(responseHeader);
+		return response;
+	}
+
+}
