759e676f4d34dd756e691f38a95e2143c3249024
zhanglh <zhanglh@asiainfo.com>
2016-09-22 09:37:14 +0800

    引入支付的handler内容

--- order-core/src/test/java/com/ai/slp/order/api/notice/PayNoticeTest.java
+++ order-core/src/test/java/com/ai/slp/order/api/notice/PayNoticeTest.java
@@ -1,5 +1,9 @@
 package com.ai.slp.order.api.notice;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,19 +12,67 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.ai.opt.base.vo.BaseResponse;
 import com.ai.slp.order.api.notice.interfaces.IPayNoticeSV;
+import com.ai.slp.order.util.NoticeUtil;
 import com.alibaba.fastjson.JSON;
+import com.upp.docking.enums.TranType;
+import com.ylink.upp.base.oxm.util.OxmHandler;
+import com.ylink.upp.oxm.entity.upp_103_001_01.GrpBody;
+import com.ylink.upp.oxm.entity.upp_103_001_01.GrpHdr;
+import com.ylink.upp.oxm.entity.upp_103_001_01.RespInfo;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "/context/core-context.xml" })
 public class PayNoticeTest {
 	@Autowired
     private IPayNoticeSV iPayNoticeSV;
+	@Autowired
+	private OxmHandler oxmHandler;
 	@Test
     public void testPayNoticeSV(){
-		String body = "<GrpBody><MerOrderId>2522477</MerOrderId><PayTranSn>321232</PayTranSn><OrderAmt>123</OrderAmt><OrderDate></OrderDate><PayStatus></PayStatus><Remark></Remark><Resv>保留域不能为空</Resv></GrpBody>";
-		String header="<GrpHdr><MerNo>10000000</MerNo><CreDtTm>20160918114034</CreDtTm><TranType>103.001.01</TranType></GrpHdr>";
-		String sign="FDSFGSDFGD";
-        BaseResponse info=iPayNoticeSV.getPayNotice(body, sign, header);
-        System.out.println("info="+JSON.toJSONString(info.getResponseHeader().getIsSuccess()));
+		GrpHdr hdr = new GrpHdr();
+		String merNo="CO20160900000009";
+		hdr.setMerNo(merNo);
+		hdr.setCreDtTm(new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()));
+		hdr.setTranType(TranType.PAY_NOTICE.getValue());
+		// 消息体
+		GrpBody body = new GrpBody();
+		body.setMerOrderId("4232342");
+		body.setOrderAmt(3122l);
+		body.setPayStatus("00");
+		body.setRemark("3231");
+		body.setResv("4234234");
+		body.setOrderDate(new Date().getTime());
+		body.setPaymentChannel("09");
+		RespInfo respInfo = new RespInfo();
+		respInfo.setGrpHdr(hdr);
+		respInfo.setGrpBody(body);
+		// 发送消息
+		String xmlMsg = null;
+		try {
+			xmlMsg = oxmHandler.marshal(respInfo);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// 加签
+		String sign = null;
+		try {
+			sign = NoticeUtil.sign(xmlMsg);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// 拼装报文头
+		String msgHeader = NoticeUtil.initMsgHeader(merNo, TranType.PAY_NOTICE.getValue());
+		/*param.put("msgHeader", msgHeader);
+		param.put("xmlBody", xmlMsg);
+		String result = null;*/
+		/*try {
+			result = NoticeUtil.sendHttpPost("http://10.19.13.13:10887/slp-order/refundnotice/notice", param, "UTF-8");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}*/
+		BaseResponse INFO= iPayNoticeSV.getPayNotice(xmlMsg, sign, msgHeader);
+        System.out.println("info="+JSON.toJSONString(INFO.getResponseHeader()));
     }
+	
 }
