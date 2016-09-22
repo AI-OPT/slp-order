88716ce8aee3cb10e884a30f8344c59961a82e69
fazhicao <409993207@qq.com>
2016-09-22 20:34:55 +0800

    修改用户信息

--- order-core/src/main/java/com/ai/slp/order/util/ChUserUtil.java
+++ order-core/src/main/java/com/ai/slp/order/util/ChUserUtil.java
@@ -13,7 +13,7 @@ import com.alibaba.fastjson.JSONObject;
 public  class ChUserUtil {
 	private static final Logger LOG = LoggerFactory.getLogger(ChUserUtil.class);
 	//获取绑定手机号
-	public static String getPhone(String id){
+	public static JSONObject getUserInfo(String id){
 		 Map<String,String> params=new HashMap<String,String>();
 		   params.put("uid", id);
 	       String url="http://10.19.13.16:28151/opaas/http/srv_up_user_getuserdetialbyuid_reg";
@@ -27,19 +27,20 @@ public  class ChUserUtil {
 				e.printStackTrace();
 			}
 	     //将返回结果，转换为JSON对象 
+		 JSONObject dataJson=null;
 	     JSONObject json=JSON.parseObject(result);
 	     String reqResultCode=json.getString("resultCode");
 	     if("000000".equals(reqResultCode)){
 	         String dataStr=(String)json.get("data");
-	         JSONObject dataJson=JSON.parseObject(dataStr);
-	         Object phone =dataJson.get("phone");
+	         dataJson=JSON.parseObject(dataStr);
+	        /* Object phone =dataJson.get("phone");
 	         if(phone!=null){
 	        	 return phone.toString();
-	         }
+	         }*/
 	     }else{
 	     	//请求过程失败
 	    	 LOG.info("请求失败,请求错误码:"+reqResultCode);
 	     }
-	     return null;
+	     return dataJson;
 	}
 }
