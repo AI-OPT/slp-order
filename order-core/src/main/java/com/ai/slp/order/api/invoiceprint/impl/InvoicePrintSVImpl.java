478b9a9b6b036c9cd87bb8d4f7c0a17709d54e4d
fazhicao <409993207@qq.com>
2016-09-24 17:22:26 +0800

    发票打印和状态修改服务

--- order-core/src/main/java/com/ai/slp/order/api/invoiceprint/impl/InvoicePrintSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/api/invoiceprint/impl/InvoicePrintSVImpl.java
@@ -1,7 +1,6 @@
 package com.ai.slp.order.api.invoiceprint.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
@@ -9,34 +8,30 @@ import com.ai.opt.base.vo.BaseResponse;
 import com.ai.opt.base.vo.ResponseHeader;
 import com.ai.opt.sdk.constants.ExceptCodeConstants;
 import com.ai.slp.order.api.invoiceprint.interfaces.IInvoicePrintSV;
-import com.ai.slp.order.api.invoiceprint.param.InvoicePrintInfosRequest;
+import com.ai.slp.order.api.invoiceprint.param.InvoiceNoticeRequest;
 import com.ai.slp.order.api.invoiceprint.param.InvoicePrintRequest;
 import com.ai.slp.order.api.invoiceprint.param.InvoicePrintResponse;
 import com.ai.slp.order.service.business.interfaces.IInvoicePrintBusiSV;
-import com.alibaba.dubbo.config.annotation.Service;
 
-@Service(validation="true")
-@Component
-public class InvoicePrintSVImpl implements IInvoicePrintSV{
+public class InvoicePrintSVImpl implements IInvoicePrintSV {
 	
 	@Autowired
 	private IInvoicePrintBusiSV invoicePrintBusiSV;
 
 	@Override
-	public InvoicePrintResponse query(InvoicePrintRequest request) throws BusinessException, SystemException {
-		InvoicePrintResponse response = invoicePrintBusiSV.invoiceQuery(request);
+	public InvoicePrintResponse queryList(InvoicePrintRequest request) throws BusinessException, SystemException {
+		InvoicePrintResponse response = invoicePrintBusiSV.queryList(request);
 		ResponseHeader header=new ResponseHeader(true, ExceptCodeConstants.Special.SUCCESS, "成功");
 		response.setResponseHeader(header);
 		return response;
 	}
 
 	@Override
-	public BaseResponse print(InvoicePrintInfosRequest request) throws BusinessException, SystemException {
-		BaseResponse response = new BaseResponse();
-		invoicePrintBusiSV.invoicePrint(request);
+	public BaseResponse updateInvoiceStatus(InvoiceNoticeRequest request) throws BusinessException, SystemException {
+		BaseResponse response=new BaseResponse();
+		invoicePrintBusiSV.updateInvoiceStatus(request);
 		ResponseHeader header=new ResponseHeader(true, ExceptCodeConstants.Special.SUCCESS, "成功");
 		response.setResponseHeader(header);
 		return response;
 	}
-
 }
