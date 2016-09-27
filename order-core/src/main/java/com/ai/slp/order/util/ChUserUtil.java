6f3beac96563d42928ae19338b77fcb0a41476b0
fazhicao <409993207@qq.com>
2016-09-27 19:52:19 +0800

    用户id获取用户信息修改

--- order-core/src/main/java/com/ai/slp/order/util/ChUserUtil.java
+++ order-core/src/main/java/com/ai/slp/order/util/ChUserUtil.java
@@ -12,17 +12,17 @@ import com.alibaba.fastjson.JSONObject;
 
 public  class ChUserUtil {
 	private static final Logger LOG = LoggerFactory.getLogger(ChUserUtil.class);
-	//获取绑定手机号
+	//获取用户信息
 	public static JSONObject getUserInfo(String id){
 		 Map<String,String> params=new HashMap<String,String>();
-		   params.put("uid", id);
-	       String url="http://10.19.13.16:28151/opaas/http/srv_up_user_getuserdetialbyuid_reg";
+		   params.put("openId", id);
+	       String url="http://10.19.13.16:28151/opaas/http/srv_up_user_getuserinfobyopenid_qry";
 	       String param=JSON.toJSONString(params);
 	       Map<String,String> mapHeader = new HashMap<String,String>();
 	       mapHeader.put("appkey", "3a83ed361ebce978731b736328a97ea8");
 	       String result ="";
 			try {
-				result = HttpClientUtil.sendPost(url, param, mapHeader);
+				result = HttpClientUtil.sendPost(url, param,mapHeader);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -38,5 +38,9 @@ public  class ChUserUtil {
 	    	 LOG.info("请求失败,请求错误码:"+reqResultCode);
 	     }
 	     return dataJson;
+	}
+	
+	public static void main(String[] args) {
+		getUserInfo("7048d255c62e4511");
 	}
 }
