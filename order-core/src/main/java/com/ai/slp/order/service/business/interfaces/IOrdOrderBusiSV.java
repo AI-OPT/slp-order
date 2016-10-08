0e891f5abbdc396a621d9f17c319106c625f0bee
zhanglh <zhanglh@asiainfo.com>
2016-10-08 13:20:45 +0800

    添加修改订单状态服务

--- order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IOrdOrderBusiSV.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IOrdOrderBusiSV.java
@@ -10,6 +10,7 @@ import com.ai.slp.order.api.orderlist.param.QueryOrderListRequest;
 import com.ai.slp.order.api.orderlist.param.QueryOrderListResponse;
 import com.ai.slp.order.api.orderlist.param.QueryOrderRequest;
 import com.ai.slp.order.api.orderlist.param.QueryOrderResponse;
+import com.ai.slp.order.dao.mapper.bo.OrdOrder;
 
 public interface IOrdOrderBusiSV {
 
@@ -23,6 +24,9 @@ public interface IOrdOrderBusiSV {
             throws BusinessException, SystemException;
     
     public BehindQueryOrderListResponse behindQueryOrderList(BehindQueryOrderListRequest orderListRequest)
+            throws BusinessException, SystemException;
+    
+    public int updateOrder(OrdOrder request)
             throws BusinessException, SystemException;
     
 }
