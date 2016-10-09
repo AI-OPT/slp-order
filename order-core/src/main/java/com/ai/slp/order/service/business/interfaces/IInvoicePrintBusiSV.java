fbc3e8d45593c7a8eb9e1c59239e9cf53affa241
fazhicao <409993207@qq.com>
2016-10-09 10:19:39 +0800

    发票打印获取信息获取

--- order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IInvoicePrintBusiSV.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IInvoicePrintBusiSV.java
@@ -5,10 +5,14 @@ import com.ai.opt.base.exception.SystemException;
 import com.ai.slp.order.api.invoiceprint.param.InvoiceNoticeRequest;
 import com.ai.slp.order.api.invoiceprint.param.InvoicePrintRequest;
 import com.ai.slp.order.api.invoiceprint.param.InvoicePrintResponse;
+import com.ai.slp.order.api.invoiceprint.param.InvoiceSubmitRequest;
+import com.ai.slp.order.api.invoiceprint.param.InvoiceSumbitResponse;
 
 public interface IInvoicePrintBusiSV {
 	
 	public InvoicePrintResponse queryList(InvoicePrintRequest request) throws BusinessException, SystemException;
 	
 	public void updateInvoiceStatus(InvoiceNoticeRequest request) throws BusinessException, SystemException;
+
+	public InvoiceSumbitResponse invoiceSubmit(InvoiceSubmitRequest request) throws BusinessException, SystemException;
 }
