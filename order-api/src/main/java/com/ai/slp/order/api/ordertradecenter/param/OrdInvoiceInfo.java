83458bff675ec4902d9181b9d5c7404f5ebf4c34
fazhicao <409993207@qq.com>
2016-10-09 10:16:59 +0800

    添加发票信息

--- order-api/src/main/java/com/ai/slp/order/api/ordertradecenter/param/OrdInvoiceInfo.java
+++ order-api/src/main/java/com/ai/slp/order/api/ordertradecenter/param/OrdInvoiceInfo.java
@@ -9,7 +9,32 @@ public class OrdInvoiceInfo implements Serializable {
     /**
      * 发票类型
      */
-    private String invoiceType;         
+    private String invoiceType; 
+    
+    /**
+     * 发票种类
+     */
+    private String invoiceKind;
+    
+    /**
+     * 购货方纳税人识别号
+     */
+    private String buyerTaxpayerNumber;
+    
+    /**
+     * 购货方开户行代码
+     */
+    private String buyerBankCode;
+    
+    /**
+     * 购货方开户行名称
+     */
+    private String buyerBankName;
+    
+    /**
+     * 购货方开户行账号
+     */
+    private String buyerBankAccount;
 
     /**
      * 发票抬头
@@ -20,6 +45,46 @@ public class OrdInvoiceInfo implements Serializable {
      * 登记打印内容
      */
     private String invoiceContent;
+    
+	public String getInvoiceKind() {
+		return invoiceKind;
+	}
+
+	public void setInvoiceKind(String invoiceKind) {
+		this.invoiceKind = invoiceKind;
+	}
+
+	public String getBuyerTaxpayerNumber() {
+		return buyerTaxpayerNumber;
+	}
+
+	public void setBuyerTaxpayerNumber(String buyerTaxpayerNumber) {
+		this.buyerTaxpayerNumber = buyerTaxpayerNumber;
+	}
+
+	public String getBuyerBankCode() {
+		return buyerBankCode;
+	}
+
+	public void setBuyerBankCode(String buyerBankCode) {
+		this.buyerBankCode = buyerBankCode;
+	}
+
+	public String getBuyerBankName() {
+		return buyerBankName;
+	}
+
+	public void setBuyerBankName(String buyerBankName) {
+		this.buyerBankName = buyerBankName;
+	}
+
+	public String getBuyerBankAccount() {
+		return buyerBankAccount;
+	}
+
+	public void setBuyerBankAccount(String buyerBankAccount) {
+		this.buyerBankAccount = buyerBankAccount;
+	}
 
 	public String getInvoiceType() {
 		return invoiceType;
