cf00bc07a58ddc99b6d2b6ff3dbad1edbc0262e6
fazhicao <409993207@qq.com>
2016-10-09 14:35:15 +0800

    判断该商品对应的订单所属业务类型的测试类

---
+++ order-core/src/test/java/com/ai/slp/order/api/aftersaleorder/impl/OrderAfterSaleJudgeSVImplTest.java
@@ -0,0 +1,31 @@
+package com.ai.slp.order.api.aftersaleorder.impl;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.ai.slp.order.api.aftersaleorder.interfaces.IOrderAfterSaleJudgeSV;
+import com.ai.slp.order.api.aftersaleorder.param.OrderJuageRequest;
+import com.ai.slp.order.api.aftersaleorder.param.OrderJuageResponse;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "/context/core-context.xml" })
+public class OrderAfterSaleJudgeSVImplTest {
+	
+	@Autowired
+	private IOrderAfterSaleJudgeSV orderAfterSaleJudgeSV;
+	@Test
+	public void testJudge() {
+		OrderJuageRequest request=new OrderJuageRequest();
+		request.setOrderId(59612492l);
+		request.setSkuId("1000000000002456");
+		request.setTenantId("changhong");
+		OrderJuageResponse response = orderAfterSaleJudgeSV.judge(request);
+		System.out.println(response);
+	}
+	
+
+}
