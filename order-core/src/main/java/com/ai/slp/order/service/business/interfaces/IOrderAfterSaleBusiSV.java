8ca1ff92c47fd610f08c6b674fa323633190424d
fazhicao <409993207@qq.com>
2016-10-11 20:15:51 +0800

    新添OFC售后状态回调通知服务

--- order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IOrderAfterSaleBusiSV.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IOrderAfterSaleBusiSV.java
@@ -2,6 +2,7 @@ package com.ai.slp.order.service.business.interfaces;
 
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
+import com.ai.slp.order.api.aftersaleorder.param.OrderOFCBackRequest;
 import com.ai.slp.order.api.aftersaleorder.param.OrderReturnRequest;
 
 public interface IOrderAfterSaleBusiSV {
@@ -11,4 +12,6 @@ public interface IOrderAfterSaleBusiSV {
 	public void exchange(OrderReturnRequest request) throws BusinessException, SystemException; 
 
 	public void refund(OrderReturnRequest request) throws BusinessException, SystemException;
+
+	public void backStateOFC(OrderOFCBackRequest request) throws BusinessException, SystemException;
 }
