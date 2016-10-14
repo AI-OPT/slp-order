a9c960f8f211c8623bccaa3c4c8f30d3a59fedd9
fazhicao <409993207@qq.com>
2016-10-14 11:01:47 +0800

    打印参数新增

---
+++ order-api/src/main/java/com/ai/slp/order/api/delivergoods/param/DeliverGoodsPrintInfoVo.java
@@ -0,0 +1,88 @@
+package com.ai.slp.order.api.delivergoods.param;
+
+import java.io.Serializable;
+
+public class DeliverGoodsPrintInfoVo implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	 /**
+    * 商户编号
+    */
+	private String skuId;
+
+	/**
+    * 商品名称
+    */
+   private String prodName;
+   
+   /**
+    * 规格
+    */
+   private String extendInfo;
+   
+   /**
+    * 价格
+    */
+   private long salePrice;
+   
+   /**
+    * 数量
+    */
+   private long buySum;
+   
+   /**
+    * 合并id
+    */
+   private long horOrderId;
+
+	public String getSkuId() {
+		return skuId;
+	}
+	
+	public void setSkuId(String skuId) {
+		this.skuId = skuId;
+	}
+	
+	public String getProdName() {
+		return prodName;
+	}
+	
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+	
+	public String getExtendInfo() {
+		return extendInfo;
+	}
+	
+	public void setExtendInfo(String extendInfo) {
+		this.extendInfo = extendInfo;
+	}
+	
+	public long getSalePrice() {
+		return salePrice;
+	}
+	
+	public void setSalePrice(long salePrice) {
+		this.salePrice = salePrice;
+	}
+	
+	public long getBuySum() {
+		return buySum;
+	}
+	
+	public void setBuySum(long buySum) {
+		this.buySum = buySum;
+	}
+	
+	public long getHorOrderId() {
+		return horOrderId;
+	}
+	
+	public void setHorOrderId(long horOrderId) {
+		this.horOrderId = horOrderId;
+	}
+   
+
+}
