7763785614ef78448a3ad35efab509a07b275ae5
fazhicao <409993207@qq.com>
2016-10-17 21:17:43 +0800

    添加费用明细修改

--- order-core/src/main/java/com/ai/slp/order/service/atom/interfaces/IOrdOdFeeProdAtomSV.java
+++ order-core/src/main/java/com/ai/slp/order/service/atom/interfaces/IOrdOdFeeProdAtomSV.java
@@ -2,6 +2,7 @@ package com.ai.slp.order.service.atom.interfaces;
 
 import java.util.List;
 
+
 import com.ai.slp.order.dao.mapper.bo.OrdOdFeeProd;
 import com.ai.slp.order.dao.mapper.bo.OrdOdFeeProdCriteria;
 
@@ -13,5 +14,7 @@ public interface IOrdOdFeeProdAtomSV {
     OrdOdFeeProd selectByOrdAndStyle(long orderId,String payStyle);
 
     List<OrdOdFeeProd> selectByOrderId(long orderId);
+    
+    int updateByExample(OrdOdFeeProd record, long orderId, String payStyle);
 
 }
