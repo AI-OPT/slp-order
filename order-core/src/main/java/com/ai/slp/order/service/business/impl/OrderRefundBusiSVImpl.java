f80fea17fffce7ab592cb2827418ebc317a3deec
fazhicao <409993207@qq.com>
2016-10-23 16:21:52 +0800

    二次审核失败之后判断子订单信息

--- order-core/src/main/java/com/ai/slp/order/service/business/impl/OrderRefundBusiSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/impl/OrderRefundBusiSVImpl.java
@@ -25,6 +25,7 @@ import com.ai.slp.order.dao.mapper.bo.OrdOdFeeTotal;
 import com.ai.slp.order.dao.mapper.bo.OrdOdProd;
 import com.ai.slp.order.dao.mapper.bo.OrdOdProdCriteria;
 import com.ai.slp.order.dao.mapper.bo.OrdOrder;
+import com.ai.slp.order.dao.mapper.bo.OrdOrderCriteria;
 import com.ai.slp.order.dao.mapper.bo.OrdOdProdCriteria.Criteria;
 import com.ai.slp.order.service.atom.interfaces.IOrdOdFeeTotalAtomSV;
 import com.ai.slp.order.service.atom.interfaces.IOrdOdProdAtomSV;
@@ -112,8 +113,7 @@ public class OrderRefundBusiSVImpl implements IOrderRefundBusiSV {
 			throw new BusinessException(ExceptCodeConstants.Special.NO_RESULT, 
 					"订单信息不存在[订单id:"+request.getOrderId()+"租户id:"+request.getTenantId()+"]");
 		}
-		/* 拒绝  改变原始订单的商品售后标识状态*/
-		this.updateProdCusServiceFlag(ordOrder);
+		
 		/* 更新订单状态和写入订单轨迹*/
 		String orgState = ordOrder.getState();
 		String newState=OrdersConstants.OrdOrder.State.AUDIT_AGAIN_FAILURE;
@@ -124,6 +124,67 @@ public class OrderRefundBusiSVImpl implements IOrderRefundBusiSV {
         ordOrder.setStateChgTime(sysDate);
         ordOrder.setOperId(request.getOperId());
         ordOrderAtomSV.updateById(ordOrder);
+        //订单业务类型为退货的话,第二次审核失败,则判断子订单信息
+        if(OrdersConstants.OrdOrder.BusiCode.UNSUBSCRIBE_ORDER.equals(ordOrder.getBusiCode())) {
+        	/* 获取子订单信息及子订单下的商品明细信息*/
+    		OrdOrder order = ordOrderAtomSV.selectByOrderId(request.getTenantId(), 
+    				ordOrder.getOrigOrderId());
+    		List<OrdOdProd> prodList = ordOdProdAtomSV.selectByOrd(request.getTenantId(), 
+    				ordOrder.getOrigOrderId());
+    		boolean cusFlag=false;
+    		for (OrdOdProd ordOdProd : prodList) {
+    			if(OrdersConstants.OrdOrder.cusServiceFlag.YES.equals(ordOdProd.getCusServiceFlag())) {
+    				cusFlag=true;
+    			}else {
+    				cusFlag=false;
+    			}
+    		}
+    		/* 获取子订单下的所有售后订单*/
+    		OrdOrderCriteria example=new OrdOrderCriteria();
+    		OrdOrderCriteria.Criteria criteria = example.createCriteria();
+    		criteria.andOrigOrderIdEqualTo(ordOrder.getOrigOrderId());
+    		criteria.andOrderIdNotEqualTo(request.getOrderId());
+    		List<OrdOrder> orderList = ordOrderAtomSV.selectByExample(example);
+    		OrdOrder parentOrder = ordOrderAtomSV.selectByOrderId(request.getTenantId(), 
+    				 order.getParentOrderId()); //父订单
+    		boolean flag=false;
+    		for (OrdOrder afterOrdOrder : orderList) {  //表示有售后订单存在
+    			String state = afterOrdOrder.getState();
+    			if(OrdersConstants.OrdOrder.State.FINISH_REFUND.equals(state)||
+    					OrdersConstants.OrdOrder.State.EXCHANGE_AUDIT.equals(state)||
+    					OrdersConstants.OrdOrder.State.REFUND_AUDIT.equals(state)||
+    					OrdersConstants.OrdOrder.State.AUDIT_FAILURE.equals(state)||
+    					OrdersConstants.OrdOrder.State.AUDIT_AGAIN_FAILURE.equals(state)) { //表示售后订单为已完成状态或者审核失败
+    				flag=true;
+    			}else {
+    				flag=false;
+    			}
+    		}
+    		//未发货状态时
+    		if(OrdersConstants.OrdOrder.State.WAIT_DISTRIBUTION.equals(order.getState())||
+    				 OrdersConstants.OrdOrder.State.WAIT_DELIVERY.equals(order.getState())||
+    				 OrdersConstants.OrdOrder.State.WAIT_SEND.equals(order.getState())) {
+    			 if(!CollectionUtil.isEmpty(orderList)) { //有售后订单 
+    				 //TODO
+    				if(flag&&cusFlag) {
+    					order.setState(OrdersConstants.OrdOrder.State.COMPLETED);
+    					ordOrderAtomSV.updateById(order);
+    					parentOrder.setState(OrdersConstants.OrdOrder.State.COMPLETED);
+    					ordOrderAtomSV.updateById(parentOrder); 
+    				} 
+    			 }
+    		 }else { //已发货状态
+    			 if(!CollectionUtil.isEmpty(orderList)&&flag) {
+    				 order.setState(OrdersConstants.OrdOrder.State.COMPLETED);
+    				 ordOrderAtomSV.updateById(order);
+    				 parentOrder.setState(OrdersConstants.OrdOrder.State.COMPLETED);
+    				 ordOrderAtomSV.updateById(parentOrder);
+    			 }
+    		 }
+        }else {
+        	/* 退款业务类型时  拒绝  改变原始订单的商品售后标识状态*/
+    		this.updateProdCusServiceFlag(ordOrder);
+        }
         // 写入订单状态变化轨迹表
         this.updateOrderState(ordOrder, orgState, newState, chgDesc, request.getOperId());
 	}
