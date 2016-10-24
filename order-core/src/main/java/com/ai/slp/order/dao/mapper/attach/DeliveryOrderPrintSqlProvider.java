59029b6ab5a91239a1815f7ed8f3ebe31904bbd3
fazhicao <409993207@qq.com>
2016-10-24 18:31:50 +0800

    修改sql语句

--- order-core/src/main/java/com/ai/slp/order/dao/mapper/attach/DeliveryOrderPrintSqlProvider.java
+++ order-core/src/main/java/com/ai/slp/order/dao/mapper/attach/DeliveryOrderPrintSqlProvider.java
@@ -7,7 +7,7 @@ public class DeliveryOrderPrintSqlProvider {
 	public String queryOrderProd(Map<String, Object> param) {
 		StringBuffer seqBuffer = new StringBuffer();
         seqBuffer.append("select od.order_id,od.tenant_id,op.prod_name,op.buy_sum,op.sku_id,op.extend_info from ord_order od,ord_od_prod op "
-        		+ "where od.order_id=op.order_id and od.user_id="+param.get("userId")
+        		+ "where od.order_id=op.order_id and od.user_id='"+param.get("userId")+"'"
         		+ " and op.sku_id="+param.get("skuId") +" and op.route_id="+param.get("routeId")
         		+" and op.order_id!="+param.get("orderId")
         		+" and op.cus_service_flag='"+param.get("cusServiceFlag")+"'"
