3166c1e8dd8702c90472ee1f817c67045b15b260
fazhicao <409993207@qq.com>
2016-10-25 22:39:56 +0800

    O2P修改服务完成,放开注释代码

--- order-core/src/main/java/com/ai/slp/order/service/business/impl/OrderPayBusiSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/impl/OrderPayBusiSVImpl.java
@@ -834,8 +834,7 @@ public class OrderPayBusiSVImpl implements IOrderPayBusiSV {
 			if(OrdersConstants.OrdOdFeeProd.PayStyle.JF.equals(ordOdFeeProd.getPayStyle())) {
 				subOrdOdFeeProd.setPayStyle(ordOdFeeProd.getPayStyle());
 				subOrdOdFeeProd.setPaidFee(ordOdProd.getJfFee());
-				//String rate = JfAndAmountExchangeUtil.getRate(parentOrdOrder.getAccountId());
-				String rate = "100:1";
+				String rate = JfAndAmountExchangeUtil.getRate(parentOrdOrder.getAccountId());
 				if(!StringUtil.isBlank(rate)) {
 					String[] split = rate.split(":");
 					BigDecimal JfAmout=BigDecimal.valueOf(ordOdProd.getJfFee()).divide(new BigDecimal(split[0]),
@@ -902,8 +901,7 @@ public class OrderPayBusiSVImpl implements IOrderPayBusiSV {
     		if(OrdersConstants.OrdOdFeeProd.PayStyle.JF.equals(ordOdFeeProd.getPayStyle())) {
 				subOrdOdFeeProd.setPayStyle(ordOdFeeProd.getPayStyle());
 				subOrdOdFeeProd.setPaidFee(ordOdFeeProd.getPaidFee()+ordOdProd.getJfFee());
-				//String rate = JfAndAmountExchangeUtil.getRate(parentOrdOrder.getAccountId());
-				String rate = "100:1";
+				String rate = JfAndAmountExchangeUtil.getRate(parentOrdOrder.getAccountId());
 				if(!StringUtil.isBlank(rate)) {
 					String[] split = rate.split(":");
 					BigDecimal JfAmout=BigDecimal.valueOf(subOrdOdFeeProd.getPaidFee()).divide(new BigDecimal(split[0]),
