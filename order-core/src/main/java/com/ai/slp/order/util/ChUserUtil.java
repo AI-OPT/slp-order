3e3fced08c38aa87efe4c8e946034a58b52de17c
fazhicao <409993207@qq.com>
2016-10-27 20:02:01 +0800

    调取O2P解析封装类

--- order-core/src/main/java/com/ai/slp/order/util/ChUserUtil.java
+++ order-core/src/main/java/com/ai/slp/order/util/ChUserUtil.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.ai.opt.sdk.dubbo.util.HttpClientUtil;
+import com.ai.opt.sdk.util.ParseO2pDataUtil;
 import com.ai.slp.order.constants.OrdersConstants;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -15,33 +16,29 @@ public class ChUserUtil {
 	private static final Logger LOG = LoggerFactory.getLogger(ChUserUtil.class);
 	//获取用户信息
 	public static JSONObject getUserInfo(String id){
-		 Map<String,String> params=new HashMap<String,String>();
-		   params.put("openId", id);
-	       String param=JSON.toJSONString(params);
-	       Map<String,String> mapHeader = new HashMap<String,String>();
-	       mapHeader.put("appkey", OrdersConstants.OFC_APPKEY);
-	       String result ="";
-			try {
-				result = HttpClientUtil.sendPost(OrdersConstants.USER_URL, param,mapHeader);
-			} catch (Exception e) {
-				e.printStackTrace();
+	   Map<String,String> params=new HashMap<String,String>();
+	   params.put("openId", id);
+       String param=JSON.toJSONString(params);
+       Map<String,String> mapHeader = new HashMap<String,String>();
+       mapHeader.put("appkey", OrdersConstants.OFC_APPKEY);
+       String result ="";
+       JSONObject object =null;
+		try {
+			result = HttpClientUtil.sendPost(OrdersConstants.USER_URL, param,mapHeader);
+			object = ParseO2pDataUtil.getData(result);
+			Object obj = object.get("resultCode");
+			if(obj!=null) {
+				return null;
 			}
-	     //将返回结果，转换为JSON对象 
-		 JSONObject dataJson=null;
-		 JSONObject data=null;
-	     JSONObject json=JSON.parseObject(result);
-	     String reqResultCode=json.getString("resultCode");
-	     if("000000".equals(reqResultCode)){
-	         String dataStr=(String)json.get("data");
-	         data=JSON.parseObject(dataStr);
-	         dataJson = (JSONObject) data.get("data");
-	         if(dataJson==null) {
-	        	 return dataJson; 
-	         }
-	     }else{
-	     	//请求过程失败
-	    	 LOG.info("请求失败,请求错误码:"+reqResultCode);
-	     }
-	     return dataJson;
+		} catch (Exception e) {
+			e.printStackTrace();
+			LOG.error("请求出现错误!");
+		}
+		return object;
+	}
+	
+	public static void main(String[] args) {
+		JSONObject object = getUserInfo("7048d255c62e4511");
+		System.out.println(object);
 	}
 }
--- order-core/src/main/java/com/ai/slp/order/util/JfAndAmountExchangeUtil.java
+++ order-core/src/main/java/com/ai/slp/order/util/JfAndAmountExchangeUtil.java
@@ -1,5 +1,6 @@
 package com.ai.slp.order.util;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.ai.opt.sdk.dubbo.util.HttpClientUtil;
+import com.ai.opt.sdk.util.ParseO2pDataUtil;
+import com.ai.opt.sdk.util.StringUtil;
 import com.ai.slp.order.constants.OrdersConstants;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -28,35 +31,23 @@ public class JfAndAmountExchangeUtil {
 	       mapHeader.put("appkey", OrdersConstants.OFC_APPKEY);
 	       mapHeader.put("sign", OrdersConstants.OFC_SIGN);
 	       String result ="";
+	       Object rate=null;
 			try {
 				result = HttpClientUtil.sendPost(OrdersConstants.INTEGRAL_RATE_URL, param,mapHeader);
+				JSONObject object = ParseO2pDataUtil.getData(result);
+				Object obj = object.get("resultCode");
+				if(obj!=null) {
+					return null;
+				}
+				rate = object.get("rate");
 			} catch (Exception e) {
 				e.printStackTrace();
+				LOG.error("请求出现错误!");
 			}
-	     //将返回结果，转换为JSON对象 
-		 JSONObject dataJson=null;
-		 JSONObject data=null;
-		 Object rate=null;
-	     JSONObject json=JSON.parseObject(result);
-	     String reqResultCode=json.getString("resultCode");
-	     if("000000".equals(reqResultCode)){
-	         String dataStr=(String)json.get("data");
-	         dataJson=JSON.parseObject(dataStr);
-	         data = (JSONObject) dataJson.get("data");
-	         if(data==null) {
-	        	 rate=null; 
-	         }else {
-	        	 //获取比例
-	        	 rate =data.get("rate");
-	         }
-	     }else{
-	     	//请求过程失败
-	    	 LOG.info("请求失败,请求错误码:"+reqResultCode);
-	     }
-		return rate==null?null:rate.toString();
+			return rate==null?null:rate.toString();
 	}
 	
-/*	public static void main(String[] args) {
+	public static void main(String[] args) {
 		String rate = getRate("jfat1.201609256101549914_0001");
 		if(!StringUtil.isBlank(rate)) {
 			String[] split = rate.split(":");
@@ -65,5 +56,5 @@ public class JfAndAmountExchangeUtil {
 			BigDecimal JfAmout=BigDecimal.valueOf(983).divide(new BigDecimal(s),5,BigDecimal.ROUND_HALF_UP);
 			System.out.println(JfAmout.divide(new BigDecimal(s1),2,BigDecimal.ROUND_HALF_UP));
 		}
-	}*/
+	}
 }
