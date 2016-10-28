725e0f97aafd566dca06ac43f9eb467894977147
fazhicao <409993207@qq.com>
2016-10-28 10:08:57 +0800

    添加批次号查询

--- order-core/src/main/java/com/ai/slp/order/service/atom/impl/OrdOrderAtomSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/service/atom/impl/OrdOrderAtomSVImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Component;
 
+import com.ai.slp.order.constants.OrdersConstants;
 import com.ai.slp.order.dao.mapper.bo.OrdOrder;
 import com.ai.slp.order.dao.mapper.bo.OrdOrderCriteria;
 import com.ai.slp.order.dao.mapper.factory.MapperFactory;
@@ -50,16 +51,17 @@ public class OrdOrderAtomSVImpl implements IOrdOrderAtomSV {
 	@Override
 	public void updateStateByOrderId(String tenantId, Long orderId,String state) {
 		OrdOrder record = new OrdOrder();
-		//
 		record.setState(state);
 		record.setOrderId(orderId);
-		//
-//		OrdOrderCriteria example = new OrdOrderCriteria();
-//		OrdOrderCriteria.Criteria criteria = example.createCriteria();
-//		criteria.andTenantIdEqualTo(tenantId);
-//		criteria.andOrderIdEqualTo(orderId);
-		//
 		MapperFactory.getOrdOrderMapper().updateByPrimaryKeySelective(record);
+	}
+
+	@Override
+	public List<OrdOrder> selectByBatchNo(long orderId,String tenantId, long batchNo) {
+		OrdOrderCriteria example = new OrdOrderCriteria();
+        example.createCriteria().andTenantIdEqualTo(tenantId).andBatchNoEqualTo(batchNo).andOrderIdNotEqualTo(orderId)
+        .andCusServiceFlagEqualTo(OrdersConstants.OrdOrder.cusServiceFlag.NO);
+        return MapperFactory.getOrdOrderMapper().selectByExample(example);
 	}
 	
 }
