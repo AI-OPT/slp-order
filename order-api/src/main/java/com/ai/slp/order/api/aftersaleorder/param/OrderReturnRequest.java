6c89a9207e348a1785630737413cc3c2442d33c1
fazhicao <409993207@qq.com>
2016-11-06 22:16:54 +0800

    添加字段

--- order-api/src/main/java/com/ai/slp/order/api/aftersaleorder/param/OrderReturnRequest.java
+++ order-api/src/main/java/com/ai/slp/order/api/aftersaleorder/param/OrderReturnRequest.java
@@ -25,7 +25,22 @@ public class OrderReturnRequest extends BaseInfo{
 	 * 受理工号
 	 */
 	private String operId;
-
+	
+	/**
+	 * 退货 换货 退款理由
+	 */
+	private String afterSaleReason;
+	
+	/**
+	 * 图片id
+	 */
+	private String imageId;
+	
+	/**
+	 * 图片类型
+	 */
+	private String imageType;
+	
 	public Long getOrderId() {
 		return orderId;
 	}
@@ -56,5 +71,29 @@ public class OrderReturnRequest extends BaseInfo{
 
 	public void setProdSum(long prodSum) {
 		this.prodSum = prodSum;
+	}
+
+	public String getAfterSaleReason() {
+		return afterSaleReason;
+	}
+
+	public void setAfterSaleReason(String afterSaleReason) {
+		this.afterSaleReason = afterSaleReason;
+	}
+
+	public String getImageId() {
+		return imageId;
+	}
+
+	public void setImageId(String imageId) {
+		this.imageId = imageId;
+	}
+
+	public String getImageType() {
+		return imageType;
+	}
+
+	public void setImageType(String imageType) {
+		this.imageType = imageType;
 	}
 }
