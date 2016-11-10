bddda96cd0cff76415e17d975988f8cfc88c1dcb
fazhicao <409993207@qq.com>
2016-11-10 17:53:30 +0800

    sql语句返回信息修改

--- order-core/src/main/java/com/ai/slp/order/dao/mapper/attach/OrdOdInvoiceAttachMapper.java
+++ order-core/src/main/java/com/ai/slp/order/dao/mapper/attach/OrdOdInvoiceAttachMapper.java
@@ -3,6 +3,8 @@ package com.ai.slp.order.dao.mapper.attach;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.SelectProvider;
 
 import com.ai.slp.order.dao.mapper.bo.OrdOdInvoice;
@@ -14,7 +16,17 @@ public interface OrdOdInvoiceAttachMapper {
 			@Param("orderId") Long orderId, @Param("tenantId") String tenantId,
 			@Param("invoiceTitle") String invoiceTitle, @Param("invoiceStatu") String invoiceStatus);
 	
+	
 	@SelectProvider(type = OrdOdInvoiceSqlProvider.class, method = "queryInvoice")
+	@Results({ @Result(id = true, property = "orderId", column = "order_id"),
+         @Result(property = "tenantId", column = "tenant_id"),
+         @Result(property = "invoiceType", column = "invoice_type"),
+         @Result(property = "invoiceTitle", column = "invoice_title"),
+         @Result(property = "invoiceContent", column = "invoice_content"),
+         @Result(property = "invoiceStatus", column = "invoice_status"),
+         @Result(property = "invoiceId", column = "invoice_id"),
+         @Result(property = "invoiceNum", column = "invoice_num")
+       })
 	public List<OrdOdInvoice> selectList(@Param("subFlag") String subFlag,@Param("pageCount") Integer pageCount, 
 			@Param("pageSize") Integer pageSize, 
 			@Param("orderId") Long orderId, @Param("tenantId") String tenantId,
