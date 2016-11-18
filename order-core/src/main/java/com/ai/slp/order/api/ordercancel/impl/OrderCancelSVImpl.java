942970a677fe14d474615e1712c39300f7a090d4
fazhicao <409993207@qq.com>
2016-11-18 14:30:53 +0800

    修改日志类型

--- order-core/src/main/java/com/ai/slp/order/api/ordercancel/impl/OrderCancelSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/api/ordercancel/impl/OrderCancelSVImpl.java
@@ -4,8 +4,8 @@ import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.List;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -30,7 +30,7 @@ import com.alibaba.dubbo.config.annotation.Service;
 @Component
 public class OrderCancelSVImpl implements IOrderCancelSV {
     
-    private static final Log log = LogFactory.getLog(OrderCancelSVImpl.class);
+    private static final Logger log = LoggerFactory.getLogger(OrderCancelSVImpl.class);
 
     @Autowired
     private IOrdOrderAtomSV ordOrderAtomSV;
