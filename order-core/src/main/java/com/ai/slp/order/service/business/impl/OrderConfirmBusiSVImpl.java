94464165dcf905a8f75da099e40bb55d02a7450a
fazhicao <409993207@qq.com>
2016-11-25 22:22:43 +0800

    订单确认服务修改

--- order-core/src/main/java/com/ai/slp/order/service/business/impl/OrderConfirmBusiSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/impl/OrderConfirmBusiSVImpl.java
@@ -1,6 +1,7 @@
 package com.ai.slp.order.service.business.impl;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,11 +12,13 @@ import org.springframework.transaction.annotation.Transactional;
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
 import com.ai.opt.sdk.constants.ExceptCodeConstants;
+import com.ai.opt.sdk.util.CollectionUtil;
 import com.ai.opt.sdk.util.DateUtil;
 import com.ai.slp.order.api.orderconfirm.param.OrderConfirmRequest;
 import com.ai.slp.order.constants.OrdersConstants;
 import com.ai.slp.order.constants.OrdersConstants.OrdOdStateChg;
 import com.ai.slp.order.dao.mapper.bo.OrdOrder;
+import com.ai.slp.order.dao.mapper.bo.OrdOrderCriteria;
 import com.ai.slp.order.service.atom.interfaces.IOrdOrderAtomSV;
 import com.ai.slp.order.service.business.interfaces.IOrderConfirmBusiSV;
 import com.ai.slp.order.service.business.interfaces.IOrderFrameCoreSV;
@@ -47,12 +50,11 @@ public class OrderConfirmBusiSVImpl implements IOrderConfirmBusiSV {
 			throw new BusinessException(ExceptCodeConstants.Special.NO_RESULT, 
 					"订单信息不存在[订单id:"+request.getOrderId()+"租户id:"+request.getTenantId()+"]");
 		}
-		this.updateOrderState(request.getTenantId(), DateUtil.getSysDate(), request.getOrderId());
+		this.updateOrderState(ordOrder, DateUtil.getSysDate());
 	}
 	
 	
-	private void updateOrderState(String tenantId, Timestamp sysDate, long orderId) {
-        OrdOrder ordOrder = ordOrderAtomSV.selectByOrderId(tenantId, orderId);
+	private void updateOrderState(OrdOrder ordOrder, Timestamp sysDate) {
         String orgState = ordOrder.getState();
         if(!OrdersConstants.OrdOrder.State.WAIT_CONFIRM.equals(orgState)) {
         	throw new BusinessException("", "订单的状态不是待确认,无法修改");
@@ -62,21 +64,40 @@ public class OrderConfirmBusiSVImpl implements IOrderConfirmBusiSV {
         ordOrder.setState(newState2);
         ordOrder.setStateChgTime(sysDate);
         //子订单写入订单状态变化轨迹表 (待确认-已确认)
-        orderFrameCoreSV.ordOdStateChg(orderId, tenantId, orgState, newState1,
+        orderFrameCoreSV.ordOdStateChg(ordOrder.getOrderId(), ordOrder.getTenantId(), orgState, newState1,
         		OrdOdStateChg.ChgDesc.ORDER_TO_FINISH_CONFIRM, null, null, null, sysDate);
         ordOrderAtomSV.updateById(ordOrder);
         //子订单写入订单状态变化轨迹表 (已确认-完成)
-        orderFrameCoreSV.ordOdStateChg(orderId, tenantId, newState1, newState2,
+        orderFrameCoreSV.ordOdStateChg(ordOrder.getOrderId(), ordOrder.getTenantId(), newState1, newState2,
                 OrdOdStateChg.ChgDesc.ORDER_TO_COMPLETED, null, null, null, sysDate);
-        //父订单
-        OrdOrder pOrder = ordOrderAtomSV.selectByOrderId(tenantId, ordOrder.getParentOrderId());
-        String pOldstate = pOrder.getState();
-        pOrder.setState(newState2);
-        pOrder.setStateChgTime(sysDate);
-        ordOrderAtomSV.updateById(pOrder);
-        //父订单写入订单状态变化轨迹表 (已支付-完成)
-        orderFrameCoreSV.ordOdStateChg(pOrder.getOrderId(), tenantId, pOldstate, newState2,
-                OrdOdStateChg.ChgDesc.ORDER_TO_COMPLETED, null, null, null, sysDate);
+        //父订单下的其它子订单
+        OrdOrderCriteria example = new OrdOrderCriteria();
+        OrdOrderCriteria.Criteria criteria = example.createCriteria();
+        criteria.andTenantIdEqualTo(ordOrder.getTenantId()).andOrderIdNotEqualTo(ordOrder.getOrderId());
+        criteria.andParentOrderIdEqualTo(ordOrder.getParentOrderId());
+        criteria.andBusiCodeEqualTo(OrdersConstants.OrdOrder.BusiCode.NORMAL_ORDER);
+        List<OrdOrder> childOrders = ordOrderAtomSV.selectByExample(example);
+        boolean flag=true;
+	    if(!CollectionUtil.isEmpty(childOrders)) {
+	    	for (OrdOrder order : childOrders) {
+	    		//其它子订单状态不是'完成'
+				if(!newState2.equals(order.getState())) {
+					flag=false;
+					break;
+				}
+			}
+	    }
+	    if(flag) {
+	    	//父订单
+	    	OrdOrder pOrder = ordOrderAtomSV.selectByOrderId(ordOrder.getTenantId(), ordOrder.getParentOrderId());
+	    	String pOldstate = pOrder.getState();
+	    	pOrder.setState(newState2);
+	    	pOrder.setStateChgTime(sysDate);
+	    	ordOrderAtomSV.updateById(pOrder);
+	    	//父订单写入订单状态变化轨迹表 (已支付-完成)
+	    	orderFrameCoreSV.ordOdStateChg(pOrder.getOrderId(), ordOrder.getTenantId(), pOldstate, newState2,
+	    			OrdOdStateChg.ChgDesc.ORDER_TO_COMPLETED, null, null, null, sysDate);
+	    } 
     }
 
 }
