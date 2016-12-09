4a3f0e251a84f76a7df9585105154e7b2e7145ab
fazhicao <409993207@qq.com>
2016-12-09 18:11:29 +0800

    清除冗余代码

--- order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IOrdOrderBusiSV.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IOrdOrderBusiSV.java
@@ -4,25 +4,15 @@ import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
 import com.ai.slp.order.api.orderlist.param.BehindQueryOrderListRequest;
 import com.ai.slp.order.api.orderlist.param.BehindQueryOrderListResponse;
-import com.ai.slp.order.api.orderlist.param.QueryApiOrderRequest;
-import com.ai.slp.order.api.orderlist.param.QueryApiOrderResponse;
-import com.ai.slp.order.api.orderlist.param.QueryOrderListRequest;
-import com.ai.slp.order.api.orderlist.param.QueryOrderListResponse;
 import com.ai.slp.order.api.orderlist.param.QueryOrderRequest;
 import com.ai.slp.order.api.orderlist.param.QueryOrderResponse;
 import com.ai.slp.order.dao.mapper.bo.OrdOrder;
 
 public interface IOrdOrderBusiSV {
 
-    public QueryOrderListResponse queryOrderList(QueryOrderListRequest orderListRequest)
-            throws BusinessException, SystemException;
-
     public QueryOrderResponse queryOrder(QueryOrderRequest orderRequest) throws BusinessException,
             SystemException;
 
-    public QueryApiOrderResponse queryApiOrder(QueryApiOrderRequest orderRequest)
-            throws BusinessException, SystemException;
-    
     public BehindQueryOrderListResponse behindQueryOrderList(BehindQueryOrderListRequest orderListRequest)
             throws BusinessException, SystemException;
     
