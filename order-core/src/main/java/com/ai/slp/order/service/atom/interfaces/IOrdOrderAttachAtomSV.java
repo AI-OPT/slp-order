eabac0ccdb6904c82625104a413eccfb4bffc53c
fazhicao <409993207@qq.com>
2016-12-09 18:11:04 +0800

    清除冗余代码

--- order-core/src/main/java/com/ai/slp/order/service/atom/interfaces/IOrdOrderAttachAtomSV.java
+++ order-core/src/main/java/com/ai/slp/order/service/atom/interfaces/IOrdOrderAttachAtomSV.java
@@ -3,9 +3,7 @@ package com.ai.slp.order.service.atom.interfaces;
 import java.util.List;
 
 import com.ai.slp.order.api.orderlist.param.BehindQueryOrderListRequest;
-import com.ai.slp.order.api.orderlist.param.QueryOrderListRequest;
 import com.ai.slp.order.dao.mapper.attach.BehindOrdOrderAttach;
-import com.ai.slp.order.dao.mapper.attach.OrdOrderAttach;
 
 /**
  * 
@@ -16,23 +14,6 @@ import com.ai.slp.order.dao.mapper.attach.OrdOrderAttach;
  *
  */
 public interface IOrdOrderAttachAtomSV {
-	
-	/**
-	 * 查询订单信息
-	 * @param orderListRequest
-	 * @return
-	 */
-	public List<OrdOrderAttach> queryOrderBySearch(String subFlag,QueryOrderListRequest orderListRequest,String states);
-	
-	/**
-	 * 查询订单数量
-	 * @param subFlag
-	 * @param orderListRequest
-	 * @return
-	 */
-	public int queryCount(String subFlag,QueryOrderListRequest orderListRequest,String states);
-	
-	
 	
 	/**
 	 * 运营后台查询订单数量
