91c44fe79e1ffb37c5874e2f9a307aa5d6052fc8
fazhicao <409993207@qq.com>
2016-12-09 18:10:56 +0800

    清除冗余代码

--- order-core/src/main/java/com/ai/slp/order/service/atom/impl/OrdOrderAttachAtomSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/service/atom/impl/OrdOrderAttachAtomSVImpl.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.ai.slp.order.api.orderlist.param.BehindQueryOrderListRequest;
-import com.ai.slp.order.api.orderlist.param.QueryOrderListRequest;
 import com.ai.slp.order.dao.mapper.attach.BehindOrdOrderAttach;
-import com.ai.slp.order.dao.mapper.attach.OrdOrderAttach;
 import com.ai.slp.order.dao.mapper.attach.OrdOrderAttachMapper;
 import com.ai.slp.order.service.atom.interfaces.IOrdOrderAttachAtomSV;
 
@@ -17,25 +15,6 @@ public class OrdOrderAttachAtomSVImpl implements IOrdOrderAttachAtomSV {
 
     @Autowired
     private OrdOrderAttachMapper ordOrderAttachMapper;
-
-    @Override
-    public List<OrdOrderAttach> queryOrderBySearch(String subFlag,
-            QueryOrderListRequest orderListRequest, String states) {
-        return ordOrderAttachMapper.getOrdOrder(subFlag, (orderListRequest.getPageNo() - 1)
-                * orderListRequest.getPageSize(), orderListRequest.getPageSize(),
-                orderListRequest.getUserId(), orderListRequest.getOrderType(),
-                orderListRequest.getOrderId(), orderListRequest.getPayStyle(),
-                orderListRequest.getTenantId(), states, orderListRequest.getOrderTimeBegin(),
-                orderListRequest.getOrderTimeEnd());
-    }
-
-    @Override
-    public int queryCount(String subFlag, QueryOrderListRequest orderListRequest, String states) {
-        return ordOrderAttachMapper.getCount(subFlag, orderListRequest.getUserId(),
-                orderListRequest.getOrderType(), orderListRequest.getOrderId(),
-                orderListRequest.getPayStyle(), orderListRequest.getTenantId(), states,
-                orderListRequest.getOrderTimeBegin(), orderListRequest.getOrderTimeEnd());
-    }
 
 	@Override
 	public int behindQueryCount(BehindQueryOrderListRequest orderListRequest, String states) {
