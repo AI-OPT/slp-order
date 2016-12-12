8390fff097bda15d14918c144d0f2d45675f1ae7
fazhicao <409993207@qq.com>
2016-12-12 17:57:00 +0800

    清除冗余代码

--- order-core/src/main/java/com/ai/slp/order/service/business/impl/OrderReturnGoodsBusiSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/impl/OrderReturnGoodsBusiSVImpl.java
@@ -4,9 +4,6 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
