62fe3d2a17c40b8b2e5fbd6e116ccab5320cfdaf
fazhicao <409993207@qq.com>
2016-12-27 10:34:36 +0800

    添加注释

--- order-api/src/main/java/com/ai/slp/order/api/orderrule/param/OrderMonitorBeforResponse.java
+++ order-api/src/main/java/com/ai/slp/order/api/orderrule/param/OrderMonitorBeforResponse.java
@@ -4,15 +4,15 @@ import java.io.Serializable;
 
 import com.ai.opt.base.vo.BaseResponse;
 
+/**
+ * 订单下单前监控返回信息
+ */
 public class OrderMonitorBeforResponse extends BaseResponse implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	private String ifWarning;
-	private String warningType;
-	private String warningDesc;
+	private String ifWarning; //是否预警
+	private String warningType;//预警类型
+	private String warningDesc;//预警描述
 
 	public String getWarningDesc() {
 		return warningDesc;
