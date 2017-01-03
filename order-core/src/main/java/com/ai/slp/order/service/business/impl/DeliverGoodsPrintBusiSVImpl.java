60263f4561038353678fe61aa8b243f94e9628d6
fazhicao <409993207@qq.com>
2017-01-03 10:10:15 +0800

    1

--- order-core/src/main/java/com/ai/slp/order/service/business/impl/DeliverGoodsPrintBusiSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/impl/DeliverGoodsPrintBusiSVImpl.java
@@ -2,7 +2,6 @@ package com.ai.slp.order.service.business.impl;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
