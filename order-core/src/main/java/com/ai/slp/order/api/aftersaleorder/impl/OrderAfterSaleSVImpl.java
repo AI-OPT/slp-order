57cb496702ca0b9581f865014f5f0c18e7a70ef6
fazhicao <409993207@qq.com>
2017-02-06 17:31:17 +0800

    添加消息队列模式

--- order-core/src/main/java/com/ai/slp/order/api/aftersaleorder/impl/OrderAfterSaleSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/api/aftersaleorder/impl/OrderAfterSaleSVImpl.java
@@ -8,12 +8,16 @@ import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
 import com.ai.opt.base.vo.BaseResponse;
 import com.ai.opt.base.vo.ResponseHeader;
+import com.ai.opt.sdk.components.mds.MDSClientFactory;
 import com.ai.opt.sdk.constants.ExceptCodeConstants;
 import com.ai.slp.order.api.aftersaleorder.interfaces.IOrderAfterSaleSV;
 import com.ai.slp.order.api.aftersaleorder.param.OrderOFCBackRequest;
 import com.ai.slp.order.api.aftersaleorder.param.OrderReturnRequest;
+import com.ai.slp.order.constants.OrdersConstants;
 import com.ai.slp.order.service.business.interfaces.IOrderAfterSaleBusiSV;
+import com.ai.slp.order.util.MQConfigUtil;
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.fastjson.JSON;
 
 @Service(validation = "true")
 @Component
@@ -24,32 +28,75 @@ public class OrderAfterSaleSVImpl implements IOrderAfterSaleSV {
 	
 	@Override
 	public BaseResponse back(OrderReturnRequest request) throws BusinessException, SystemException {
-		BaseResponse response =new BaseResponse();
-		orderAfterSaleBusiSV.back(request);
-        ResponseHeader responseHeader = new ResponseHeader(true,
-                ExceptCodeConstants.Special.SUCCESS, "成功");
-        response.setResponseHeader(responseHeader);
-        return response;
+		boolean ccsMqFlag=false;
+    	//从配置中心获取ccsMqFlag
+    	ccsMqFlag=MQConfigUtil.getCCSMqFlag();
+    	//非消息模式下，同步调用服务
+		if(!ccsMqFlag) {
+			BaseResponse response =new BaseResponse();
+			orderAfterSaleBusiSV.back(request);
+			ResponseHeader responseHeader = new ResponseHeader(true,
+					ExceptCodeConstants.Special.SUCCESS, "成功");
+			response.setResponseHeader(responseHeader);
+			return response;
+		}else {
+			//消息模式下，异步调用服务
+			BaseResponse response =new BaseResponse();
+			//发送消息
+			MDSClientFactory.getSenderClient(OrdersConstants.MDSNS.MDS_NS_ORDER_TOPIC).send(JSON.toJSONString(request), 0);
+			ResponseHeader responseHeader = new ResponseHeader(true,
+					ExceptCodeConstants.Special.SUCCESS, "成功");
+			response.setResponseHeader(responseHeader);
+			return response;
+		}
 	}
 
 	@Override
 	public BaseResponse exchange(OrderReturnRequest request) throws BusinessException, SystemException {
-		BaseResponse response =new BaseResponse();
-		orderAfterSaleBusiSV.exchange(request);
-        ResponseHeader responseHeader = new ResponseHeader(true,
-                ExceptCodeConstants.Special.SUCCESS, "成功");
-        response.setResponseHeader(responseHeader);
-        return response;
+		boolean ccsMqFlag=false;
+		ccsMqFlag = MQConfigUtil.getCCSMqFlag();
+		if(!ccsMqFlag) {
+			//非消息模式下,同步调用服务
+			BaseResponse response =new BaseResponse();
+			orderAfterSaleBusiSV.exchange(request);
+			ResponseHeader responseHeader = new ResponseHeader(true,
+					ExceptCodeConstants.Special.SUCCESS, "成功");
+			response.setResponseHeader(responseHeader);
+			return response;
+		}else {
+			//消息模式下,异步调用服务
+			BaseResponse response =new BaseResponse();
+			//发送消息
+			MDSClientFactory.getSenderClient(OrdersConstants.MDSNS.MDS_NS_ORDER_TOPIC).send(JSON.toJSONString(request), 0);
+			ResponseHeader responseHeader = new ResponseHeader(true,
+					ExceptCodeConstants.Special.SUCCESS, "成功");
+			response.setResponseHeader(responseHeader);
+			return response;
+		}
 	}
 
 	@Override
 	public BaseResponse refund(OrderReturnRequest request) throws BusinessException, SystemException {
-		BaseResponse response =new BaseResponse();
-		orderAfterSaleBusiSV.refund(request);
-        ResponseHeader responseHeader = new ResponseHeader(true,
-                ExceptCodeConstants.Special.SUCCESS, "成功");
-        response.setResponseHeader(responseHeader);
-        return response;
+		boolean ccsMqFlag=false;
+		ccsMqFlag = MQConfigUtil.getCCSMqFlag();
+		//非消息模式
+		if(!ccsMqFlag) {
+			BaseResponse response =new BaseResponse();
+			orderAfterSaleBusiSV.refund(request);
+	        ResponseHeader responseHeader = new ResponseHeader(true,
+	                ExceptCodeConstants.Special.SUCCESS, "成功");
+	        response.setResponseHeader(responseHeader);
+	        return response;
+		}else {
+			//消息模式下
+			BaseResponse response =new BaseResponse();
+			//发送消息
+			MDSClientFactory.getSenderClient(OrdersConstants.MDSNS.MDS_NS_ORDER_TOPIC).send(JSON.toJSONString(request), 0);
+	        ResponseHeader responseHeader = new ResponseHeader(true,
+	                ExceptCodeConstants.Special.SUCCESS, "成功");
+	        response.setResponseHeader(responseHeader);
+	        return response;
+		}
 	}
 
 	@Override
