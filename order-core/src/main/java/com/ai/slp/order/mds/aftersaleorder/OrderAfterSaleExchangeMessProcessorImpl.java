100b10315f98ba515137466b24422cf0daadb1ad
fazhicao <409993207@qq.com>
2017-02-06 17:31:31 +0800

    添加消息队列模式

---
+++ order-core/src/main/java/com/ai/slp/order/mds/aftersaleorder/OrderAfterSaleExchangeMessProcessorImpl.java
@@ -0,0 +1,38 @@
+package com.ai.slp.order.mds.aftersaleorder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ai.paas.ipaas.mds.IMessageProcessor;
+import com.ai.paas.ipaas.mds.vo.MessageAndMetadata;
+import com.ai.slp.order.api.aftersaleorder.param.OrderReturnRequest;
+import com.ai.slp.order.service.business.interfaces.IOrderAfterSaleBusiSV;
+import com.alibaba.fastjson.JSON;
+
+/**
+ * 购物车消息处理
+ */
+public class OrderAfterSaleExchangeMessProcessorImpl implements IMessageProcessor {
+    private static Logger logger = LoggerFactory.getLogger(OrderAfterSaleExchangeMessProcessorImpl.class);
+
+    private IOrderAfterSaleBusiSV orderAfterSaleBusiSV;
+
+    public OrderAfterSaleExchangeMessProcessorImpl(IOrderAfterSaleBusiSV orderAfterSaleBusiSV){
+        this.orderAfterSaleBusiSV = orderAfterSaleBusiSV;
+    }
+
+    @Override
+    public void process(MessageAndMetadata message) throws Exception {
+        if (null == message)
+            return;
+        String content = new String(message.getMessage(), "UTF-8");
+        logger.info("--Topic:{}\r\n----key:{}\r\n----content:{}"
+                , message.getTopic(),new String(message.getKey(), "UTF-8"),content);
+        //转换对象
+        OrderReturnRequest request = JSON.parseObject(content,OrderReturnRequest.class);
+        if (request==null)
+            return;
+        this.orderAfterSaleBusiSV.exchange(request);        
+    }
+
+}
