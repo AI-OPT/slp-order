b3d08876f89a2f486f5f3cfbf87a91cf14227d3a
fazhicao <409993207@qq.com>
2017-02-07 14:54:04 +0800

    用户消费积分返回添加消费模式

--- order-core/src/main/java/com/ai/slp/order/api/orderpay/impl/OrderPaySVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/api/orderpay/impl/OrderPaySVImpl.java
@@ -9,12 +9,16 @@ import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
 import com.ai.opt.base.vo.BaseResponse;
 import com.ai.opt.base.vo.ResponseHeader;
+import com.ai.opt.sdk.components.mds.MDSClientFactory;
 import com.ai.opt.sdk.constants.ExceptCodeConstants;
 import com.ai.slp.order.api.orderpay.interfaces.IOrderPaySV;
 import com.ai.slp.order.api.orderpay.param.OrderOidRequest;
 import com.ai.slp.order.api.orderpay.param.OrderPayRequest;
+import com.ai.slp.order.constants.OrdersConstants;
 import com.ai.slp.order.service.business.interfaces.IOrderPayBusiSV;
+import com.ai.slp.order.util.MQConfigUtil;
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.fastjson.JSON;
 
 /**
  * 订单收费服务 Date: 2016年5月24日 <br>
@@ -46,11 +50,25 @@ public class OrderPaySVImpl implements IOrderPaySV {
 
 	@Override
 	public BaseResponse returnOid(OrderOidRequest request) throws BusinessException, SystemException {
-		 BaseResponse response = new BaseResponse();
-         orderPayBusiSV.returnOid(request);
-         ResponseHeader responseHeader = new ResponseHeader(true,
-                ExceptCodeConstants.Special.SUCCESS, "成功");
-         response.setResponseHeader(responseHeader);
-	     return response;  
+		boolean ccsMqFlag=false;
+		ccsMqFlag = MQConfigUtil.getCCSMqFlag();
+		//非消息模式 同步调用服务
+		if(!ccsMqFlag) {
+			 BaseResponse response = new BaseResponse();
+	         orderPayBusiSV.returnOid(request);
+	         ResponseHeader responseHeader = new ResponseHeader(true,
+	                ExceptCodeConstants.Special.SUCCESS, "成功");
+	         response.setResponseHeader(responseHeader);
+		     return response;  
+		}else {
+			//消息模式下 异步调用服务
+			 BaseResponse response = new BaseResponse();
+			 MDSClientFactory.getSenderClient(OrdersConstants.MDSNS.MDS_NS_ORDER_TOPIC).send(JSON.toJSONString(request), 0);
+	         ResponseHeader responseHeader = new ResponseHeader(true,
+	                ExceptCodeConstants.Special.SUCCESS, "成功");
+	         response.setResponseHeader(responseHeader);
+		     return response;  
+		}
+		
 	}
 }
