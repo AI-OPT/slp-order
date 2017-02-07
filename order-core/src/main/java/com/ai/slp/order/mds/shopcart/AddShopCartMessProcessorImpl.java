bff5485c7549a55c8d2cf6f2e7c58146a5c5c553
fazhicao <409993207@qq.com>
2017-02-07 15:09:59 +0800

    购物车相关消息处理

---
+++ order-core/src/main/java/com/ai/slp/order/mds/shopcart/AddShopCartMessProcessorImpl.java
@@ -0,0 +1,40 @@
+package com.ai.slp.order.mds.shopcart;
+
+import com.ai.paas.ipaas.mds.IMessageProcessor;
+import com.ai.paas.ipaas.mds.vo.MessageAndMetadata;
+import com.ai.slp.order.api.shopcart.param.CartProd;
+import com.ai.slp.order.service.business.interfaces.IShopCartBusiSV;
+import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 添加购物车消息处理
+ */
+public class AddShopCartMessProcessorImpl implements IMessageProcessor {
+    private static Logger logger = LoggerFactory.getLogger(AddShopCartMessProcessorImpl.class);
+
+    IShopCartBusiSV shopCartBusiSV;
+
+    public AddShopCartMessProcessorImpl(IShopCartBusiSV shopCartBusiSV){
+        this.shopCartBusiSV = shopCartBusiSV;
+    }
+
+    @Override
+    public void process(MessageAndMetadata message) throws Exception {
+        if (null == message)
+            return;
+        String content = new String(message.getMessage(), "UTF-8");
+        logger.info("--Topic:{}\r\n----key:{}\r\n----content:{}"
+                , message.getTopic(),new String(message.getKey(), "UTF-8"),content);
+        //转换对象
+        CartProd cartProd = JSON.parseObject(content,CartProd.class);
+        if (cartProd==null)
+            return;
+        this.shopCartBusiSV.addCartProd(cartProd);
+    }
+
+    public void setCartProdAtomSV(IShopCartBusiSV shopCartBusiSV) {
+        this.shopCartBusiSV = shopCartBusiSV;
+    }
+}
---
+++ order-core/src/main/java/com/ai/slp/order/mds/shopcart/UpdateShopCartMessProcessorImpl.java
@@ -0,0 +1,40 @@
+package com.ai.slp.order.mds.shopcart;
+
+import com.ai.paas.ipaas.mds.IMessageProcessor;
+import com.ai.paas.ipaas.mds.vo.MessageAndMetadata;
+import com.ai.slp.order.api.shopcart.param.CartProd;
+import com.ai.slp.order.service.business.interfaces.IShopCartBusiSV;
+import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 更新购物车消息处理
+ */
+public class UpdateShopCartMessProcessorImpl implements IMessageProcessor {
+    private static Logger logger = LoggerFactory.getLogger(UpdateShopCartMessProcessorImpl.class);
+
+    IShopCartBusiSV shopCartBusiSV;
+
+    public UpdateShopCartMessProcessorImpl(IShopCartBusiSV shopCartBusiSV){
+        this.shopCartBusiSV = shopCartBusiSV;
+    }
+
+    @Override
+    public void process(MessageAndMetadata message) throws Exception {
+        if (null == message)
+            return;
+        String content = new String(message.getMessage(), "UTF-8");
+        logger.info("--Topic:{}\r\n----key:{}\r\n----content:{}"
+                , message.getTopic(),new String(message.getKey(), "UTF-8"),content);
+        //转换对象
+        CartProd cartProd = JSON.parseObject(content,CartProd.class);
+        if (cartProd==null)
+            return;
+        this.shopCartBusiSV.updateCartProd(cartProd);
+    }
+
+    public void setCartProdAtomSV(IShopCartBusiSV shopCartBusiSV) {
+        this.shopCartBusiSV = shopCartBusiSV;
+    }
+}
