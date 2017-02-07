58811ce6df221c62e4bebf9dc74f6a353a821da4
fazhicao <409993207@qq.com>
2017-02-07 14:54:31 +0800

    买家物流填写服务添加消费模式

--- order-core/src/main/java/com/ai/slp/order/api/orderstate/impl/OrderStateServiceSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/api/orderstate/impl/OrderStateServiceSVImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
 import com.ai.opt.base.vo.ResponseHeader;
+import com.ai.opt.sdk.components.mds.MDSClientFactory;
 import com.ai.opt.sdk.constants.ExceptCodeConstants;
 import com.ai.opt.sdk.util.StringUtil;
 import com.ai.slp.order.api.orderstate.interfaces.IOrderStateServiceSV;
@@ -15,8 +16,11 @@ import com.ai.slp.order.api.orderstate.param.WaitRebateRequest;
 import com.ai.slp.order.api.orderstate.param.WaitRebateResponse;
 import com.ai.slp.order.api.orderstate.param.WaitSellReceiveSureRequest;
 import com.ai.slp.order.api.orderstate.param.WaitSellReceiveSureResponse;
+import com.ai.slp.order.constants.OrdersConstants;
 import com.ai.slp.order.service.business.interfaces.IOrderStateBusiSV;
+import com.ai.slp.order.util.MQConfigUtil;
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.fastjson.JSON;
 @Service
 @Component
 public class OrderStateServiceSVImpl implements IOrderStateServiceSV {
@@ -44,24 +48,34 @@ public class OrderStateServiceSVImpl implements IOrderStateServiceSV {
 		if(StringUtil.isBlank(request.getExpressOddNumber())){
 			throw new BusinessException(ExceptCodeConstants.Special.PARAM_IS_NULL,"快递Number不能为空");
 		}
-		//
-		try{
-			response = this.orderStateBusiSV.updateWaitSellRecieveSureState(request);
+		boolean ccsMqFlag=false;
+		//从配置中心获取消息启动标识ccsMqFlag
+		ccsMqFlag = MQConfigUtil.getCCSMqFlag();
+		//非消息模式 同步调用服务
+		if(!ccsMqFlag) {
+			try{
+				response = this.orderStateBusiSV.updateWaitSellRecieveSureState(request);
+				responseHeader.setIsSuccess(true);
+				responseHeader.setResultCode("000000");
+				responseHeader.setResultMessage("修改状态成功");
+				//
+				response.setResponseHeader(responseHeader);
+			}catch(Exception e){
+				responseHeader.setResultCode("999999");
+				responseHeader.setResultMessage("修改状态失败");			
+				response.setResponseHeader(responseHeader);
+				LOG.error("修改状态失败:"+e.getMessage(),e);
+			}
+			return response;
+		}else {
+			//消息模式 异步调用服务
+			MDSClientFactory.getSenderClient(OrdersConstants.MDSNS.MDS_NS_ORDER_TOPIC).send(JSON.toJSONString(request), 0);
 			responseHeader.setIsSuccess(true);
 			responseHeader.setResultCode("000000");
 			responseHeader.setResultMessage("修改状态成功");
-			//
 			response.setResponseHeader(responseHeader);
-		}catch(Exception e){
-			//e.printStackTrace();
-			responseHeader.setResultCode("999999");
-			responseHeader.setResultMessage("修改状态失败");			
-			//
-			response.setResponseHeader(responseHeader);
-			LOG.error("修改状态失败:"+e.getMessage(),e);
+			return response;
 		}
-		
-		return response;
 	}
 
 	@Override
