b39f21276495ecb2d7b10f9b61a7735b61c5447d
fazhicao <409993207@qq.com>
2017-02-14 20:38:02 +0800

    1

--- order-core/src/main/java/com/ai/slp/order/dao/mapper/attach/OrdOrderSqlProvider.java
+++ order-core/src/main/java/com/ai/slp/order/dao/mapper/attach/OrdOrderSqlProvider.java
@@ -182,7 +182,7 @@ public class OrdOrderSqlProvider {
         seqBuffer.append("select oo.order_id,oo.state,oo.tenant_id,oo.chl_id,oo.delivery_flag,contact_tel,"
         		        		+ "oo.user_id,oo.user_name,oo.user_tel,discount_fee,adjust_fee from ord_order oo,ord_od_logistics ol "
         		        		+ "where oo.order_id IN (select * from(select oo.order_id "
-        		        		+ "from ord_order oo,ord_od_logistics ol ord_od_fee_total of where"
+        		        		+ "from ord_order oo,ord_od_logistics ol,ord_od_fee_total of where"
         		                + " oo.tenant_id= '"+ param.get("tenantId") + "' and oo.flag "
         		                		+ "in('"+ OrdersConstants.OrdOrder.Flag.UPPLATFORM + "','"+ OrdersConstants.OrdOrder.Flag.OFC_ACTUAL_TIME+"')"
         		                );
