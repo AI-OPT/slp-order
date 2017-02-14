2592fb5eb3b642f6f54afa92709fbe333c221cae
fazhicao <409993207@qq.com>
2017-02-14 20:49:27 +0800

    1

--- order-core/src/main/java/com/ai/slp/order/dao/mapper/attach/OrdOrderSqlProvider.java
+++ order-core/src/main/java/com/ai/slp/order/dao/mapper/attach/OrdOrderSqlProvider.java
@@ -19,17 +19,16 @@ public class OrdOrderSqlProvider {
         String routeId = param.containsKey("routeId") ? (String) param.get("routeId") : null;
         StringBuffer seqBuffer = new StringBuffer();
         seqBuffer.append("select oo.order_id,oo.state,oo.tenant_id,oo.chl_id,oo.delivery_flag,contact_tel,"
-        		        		+ "oo.user_id,oo.user_name,oo.user_tel,discount_fee,adjust_fee from ord_order oo,ord_od_logistics ol "
+        		        		+ "oo.user_id,oo.user_name,oo.user_tel,discount_fee,adjust_fee from ord_order oo,ord_od_logistics ol,ord_od_fee_total of "
         		        		+ "where oo.order_id IN (select * from(select oo.order_id "
-        		        		+ "from ord_order oo,ord_od_logistics ol,ord_od_fee_total of where"
+        		        		+ "from ord_order oo,ord_od_logistics ol where"
         		                + " oo.tenant_id= '"+ param.get("tenantId") + "' and oo.flag "
         		                		+ "in('"+ OrdersConstants.OrdOrder.Flag.UPPLATFORM + "','"+ OrdersConstants.OrdOrder.Flag.OFC_ACTUAL_TIME+"')"
         		                );
         seqBuffer = getConnectStr(param, seqBuffer);  
         if(StringUtil.isBlank(states)) {  //空
         		if(StringUtil.isBlank(routeId)) {
-        			seqBuffer.append(" and oo.order_id=ol.order_id and oo.PARENT_ORDER_ID=0"
-        					+ " group by oo.order_id order by oo.order_time desc limit ");
+        			seqBuffer.append(" and oo.order_id=ol.order_id and oo.PARENT_ORDER_ID=0");
         		}else {
         			seqBuffer.append(" and od.route_id = '" + routeId+"'");
                 	seqBuffer.append(" and oo.order_id=od.PARENT_ORDER_ID and oo.order_id=ol.order_id and oo.order_id=of.order_id");
