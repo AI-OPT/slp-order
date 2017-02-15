2b20de11ce6447d06732b3ac00b869c378625f82
fazhicao <409993207@qq.com>
2017-02-15 16:32:15 +0800

    优化sql

--- order-core/src/main/java/com/ai/slp/order/dao/mapper/attach/OrdOrderSqlProvider.java
+++ order-core/src/main/java/com/ai/slp/order/dao/mapper/attach/OrdOrderSqlProvider.java
@@ -19,18 +19,17 @@ public class OrdOrderSqlProvider {
         String routeId = param.containsKey("routeId") ? (String) param.get("routeId") : null;
         StringBuffer seqBuffer = new StringBuffer();
         seqBuffer.append("select oo.order_id,oo.state,oo.tenant_id,oo.chl_id,oo.delivery_flag,contact_tel,"
-        		        		+ "oo.user_id,oo.user_name,oo.user_tel,discount_fee,adjust_fee from ord_order oo,ord_od_logistics ol,ord_od_fee_total of "
-        		        		+ "where oo.order_id IN (select * from(select oo.order_id ");
+        		        		+ "oo.user_id,oo.user_name,oo.user_tel,discount_fee,adjust_fee from ord_order oo,ord_od_logistics ol,ord_od_fee_total of ");
         if(StringUtil.isBlank(states)) {  //空
         		if(StringUtil.isBlank(routeId)) {
-        			seqBuffer.append("from ord_order oo,ord_od_logistics ol where"
+        			seqBuffer.append(" where"
     		                + " oo.tenant_id= '"+ param.get("tenantId") + "' and oo.flag "
     		                		+ "in('"+ OrdersConstants.OrdOrder.Flag.UPPLATFORM + "','"+ OrdersConstants.OrdOrder.Flag.OFC_ACTUAL_TIME+"')"
     		                );
     		         seqBuffer = getConnectStr(param, seqBuffer); 
         			seqBuffer.append(" and oo.order_id=ol.order_id and oo.PARENT_ORDER_ID=0");
         		}else {
-        			seqBuffer.append("from ord_order oo,ord_order od,ord_od_logistics ol where"
+        			seqBuffer.append(" where"
     		                + " oo.tenant_id= '"+ param.get("tenantId") + "' and oo.flag "
     		                		+ "in('"+ OrdersConstants.OrdOrder.Flag.UPPLATFORM + "','"+ OrdersConstants.OrdOrder.Flag.OFC_ACTUAL_TIME+"')"
     		                );
@@ -40,7 +39,7 @@ public class OrdOrderSqlProvider {
         		}
         }else if (!StringUtil.isBlank(states) && (OrdersConstants.OrdOrder.State.WAIT_PAY.equals(states)||
         		OrdersConstants.OrdOrder.State.CANCEL.equals(states))){   //父状态
-        	seqBuffer.append("from ord_order oo,ord_od_logistics ol where"
+        	seqBuffer.append(" where"
 	                + " oo.tenant_id= '"+ param.get("tenantId") + "' and oo.flag "
 	                		+ "in('"+ OrdersConstants.OrdOrder.Flag.UPPLATFORM + "','"+ OrdersConstants.OrdOrder.Flag.OFC_ACTUAL_TIME+"')"
 	                );
@@ -59,7 +58,7 @@ public class OrdOrderSqlProvider {
         	seqBuffer.append(" and od.state in(" + states + ")");
         	seqBuffer.append(" and oo.order_id=od.PARENT_ORDER_ID and oo.order_id=ol.order_id");
         }
-        seqBuffer.append(" group by oo.order_id order by oo.ORDER_ID desc limit "
+        seqBuffer.append(" order by oo.ORDER_ID desc limit "
         + param.get("pageCount") + "," + param.get("pageSize")+") as t) and oo.order_id=ol.order_id and oo.order_id=of.order_id");
         return seqBuffer.toString();
     }
@@ -73,29 +72,29 @@ public class OrdOrderSqlProvider {
         StringBuffer seqBuffer = new StringBuffer();
         if(StringUtil.isBlank(states)) {  //空
     		if(StringUtil.isBlank(routeId)) {
-    			seqBuffer.append("select count(*) from (select oo.ORDER_ID from ord_order oo,ord_od_logistics ol where"
+    			seqBuffer.append("select count(*) from ord_order oo,ord_od_logistics ol where"
     	                + " oo.tenant_id= '"+ param.get("tenantId") + "'"
     	                +" and oo.flag in('"+ OrdersConstants.OrdOrder.Flag.UPPLATFORM + "','"+ OrdersConstants.OrdOrder.Flag.OFC_ACTUAL_TIME+"')");
     			seqBuffer = getConnectStr(param, seqBuffer);
-    			seqBuffer.append(" and oo.PARENT_ORDER_ID=0 and oo.order_id=ol.order_id group by oo.ORDER_ID) c");
+    			seqBuffer.append(" and oo.PARENT_ORDER_ID=0 and oo.order_id=ol.order_id");
     		}else {
-    			seqBuffer.append("select count(*) from (select oo.ORDER_ID from ord_order oo,ord_order od,ord_od_logistics ol where"
+    			seqBuffer.append("select count(*) from ord_order oo,ord_order od,ord_od_logistics ol where"
     	                + " oo.tenant_id= '"+ param.get("tenantId") + "'"
     	                +" and oo.flag in('"+ OrdersConstants.OrdOrder.Flag.UPPLATFORM + "','"+ OrdersConstants.OrdOrder.Flag.OFC_ACTUAL_TIME+"')");
     			seqBuffer = getConnectStr(param, seqBuffer);
     			seqBuffer.append(" and od.route_id = '" + routeId+"'");
-            	seqBuffer.append(" and oo.order_id=od.PARENT_ORDER_ID and oo.order_id=ol.order_id group by oo.order_id) c");
+            	seqBuffer.append(" and oo.order_id=od.PARENT_ORDER_ID and oo.order_id=ol.order_id");
     		}
     	}else if (!StringUtil.isBlank(states) && (OrdersConstants.OrdOrder.State.WAIT_PAY.equals(states)||
             		OrdersConstants.OrdOrder.State.CANCEL.equals(states))){   //父状态
-    			seqBuffer.append("select count(*) from (select oo.ORDER_ID from ord_order oo,ord_od_logistics ol where"
+    			seqBuffer.append("select count(*) from ord_order oo,ord_od_logistics ol where"
 	                + " oo.tenant_id= '"+ param.get("tenantId") + "'"
 	                +" and oo.flag in('"+ OrdersConstants.OrdOrder.Flag.UPPLATFORM + "','"+ OrdersConstants.OrdOrder.Flag.OFC_ACTUAL_TIME+"')");
     			seqBuffer = getConnectStr(param, seqBuffer);
             	seqBuffer.append(" and oo.state in(" + states + ")");
-            	seqBuffer.append(" and oo.order_id=ol.order_id and oo.order_id=of.order_id group by oo.order_id) c");
+            	seqBuffer.append(" and oo.order_id=ol.order_id");
          }else {
-        	 	seqBuffer.append("select count(*) from (select oo.ORDER_ID from ord_order oo,ord_order od,ord_od_logistics ol where"
+        	 	seqBuffer.append("select count(*) from ord_order oo,ord_order od,ord_od_logistics ol where"
  	                + " oo.tenant_id= '"+ param.get("tenantId") + "'"
  	               +" and oo.flag in('"+ OrdersConstants.OrdOrder.Flag.UPPLATFORM + "','"+ OrdersConstants.OrdOrder.Flag.OFC_ACTUAL_TIME+"')");
         	 	seqBuffer = getConnectStr(param, seqBuffer);
@@ -103,7 +102,7 @@ public class OrdOrderSqlProvider {
             		seqBuffer.append(" and od.route_id = '" + routeId+"'");
             	}
             	seqBuffer.append(" and od.state in(" + states + ")");
-            	seqBuffer.append(" and oo.order_id=od.PARENT_ORDER_ID and oo.order_id=ol.order_id group by oo.order_id) c");
+            	seqBuffer.append(" and oo.order_id=od.PARENT_ORDER_ID and oo.order_id=ol.order_id");
          }
          return seqBuffer.toString();
     }
