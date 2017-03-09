943a5ec412d783ecf8902d6055e376882801e4ab
fazhicao <409993207@qq.com>
2017-03-09 13:41:11 +0800

    搜索引擎接口

---
+++ order-api/src/main/java/com/ai/slp/order/api/sesdata/param/SesDataRequest.java
@@ -0,0 +1,21 @@
+package com.ai.slp.order.api.sesdata.param;
+
+import com.ai.opt.base.vo.BaseInfo;
+
+public class SesDataRequest extends BaseInfo{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	//父订单id
+	private Long parentOrderId;
+	
+	public Long getParentOrderId() {
+		return parentOrderId;
+	}
+
+	public void setParentOrderId(Long parentOrderId) {
+		this.parentOrderId = parentOrderId;
+	}
+}
