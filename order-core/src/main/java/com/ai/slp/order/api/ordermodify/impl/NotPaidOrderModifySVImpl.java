a9a47184b85c04e83ae0eec84b275cfeee1d3207
fazhicao <409993207@qq.com>
2017-03-09 13:41:40 +0800

    添加搜索引擎数据导入

--- order-core/src/main/java/com/ai/slp/order/api/ordermodify/impl/NotPaidOrderModifySVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/api/ordermodify/impl/NotPaidOrderModifySVImpl.java
@@ -11,19 +11,27 @@ import com.ai.opt.base.vo.ResponseHeader;
 import com.ai.opt.sdk.constants.ExceptCodeConstants;
 import com.ai.slp.order.api.ordermodify.interfaces.INotPaidOrderModifySV;
 import com.ai.slp.order.api.ordermodify.param.OrderModifyRequest;
+import com.ai.slp.order.api.sesdata.param.SesDataRequest;
 import com.ai.slp.order.service.business.interfaces.INotPaidOrderModifyBusiSV;
-import com.alibaba.dubbo.config.annotation.Service;
+import com.ai.slp.order.service.business.interfaces.search.IOrderIndexBusiSV;
 
-@Service(validation="true")
 @Component
 public class NotPaidOrderModifySVImpl implements INotPaidOrderModifySV{
-	
 	@Autowired
 	private INotPaidOrderModifyBusiSV notPaidOrderModifyBusiSV;
+	@Autowired
+	private IOrderIndexBusiSV orderIndexBusiSV;
+	
 	@Override
 	public BaseResponse modify(OrderModifyRequest request) throws BusinessException, SystemException {
 		BaseResponse response=new BaseResponse();
+		//1.修改金额
 		notPaidOrderModifyBusiSV.modify(request);
+		//2.导入数据到搜索引擎
+		SesDataRequest sesReq=new SesDataRequest();
+		sesReq.setTenantId(request.getTenantId());
+		sesReq.setParentOrderId(request.getOrderId());
+		this.orderIndexBusiSV.insertSesData(sesReq);
 		ResponseHeader responseHeader = new ResponseHeader(true,
                 ExceptCodeConstants.Special.SUCCESS, "成功");
 		response.setResponseHeader(responseHeader);
