d68e7d3bba0d16cf7aa28d9c97cfe5e6805f6c48
fazhicao <409993207@qq.com>
2017-03-10 10:15:37 +0800

    搜索写入事务中,保证事务的一致性

--- order-core/src/main/java/com/ai/slp/order/api/ordertradecenter/impl/OrderTradeCenterSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/api/ordertradecenter/impl/OrderTradeCenterSVImpl.java
@@ -1,7 +1,5 @@
 package com.ai.slp.order.api.ordertradecenter.impl;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,13 +9,10 @@ import com.ai.opt.base.vo.ResponseHeader;
 import com.ai.opt.sdk.components.mds.MDSClientFactory;
 import com.ai.opt.sdk.constants.ExceptCodeConstants;
 import com.ai.slp.order.api.ordertradecenter.interfaces.IOrderTradeCenterSV;
-import com.ai.slp.order.api.ordertradecenter.param.OrderResInfo;
 import com.ai.slp.order.api.ordertradecenter.param.OrderTradeCenterRequest;
 import com.ai.slp.order.api.ordertradecenter.param.OrderTradeCenterResponse;
-import com.ai.slp.order.api.sesdata.param.SesDataRequest;
 import com.ai.slp.order.constants.OrdersConstants;
 import com.ai.slp.order.service.business.interfaces.IOrdOrderTradeBusiSV;
-import com.ai.slp.order.service.business.interfaces.search.IOrderIndexBusiSV;
 import com.ai.slp.order.util.MQConfigUtil;
 import com.ai.slp.order.util.ValidateUtils;
 import com.alibaba.fastjson.JSON;
@@ -28,9 +23,6 @@ public class OrderTradeCenterSVImpl implements IOrderTradeCenterSV {
     @Autowired
     private IOrdOrderTradeBusiSV ordOrderTradeBusiSV;
     
-    @Autowired
-    private IOrderIndexBusiSV orderIndexBusiSV;
-
     @Override
     public OrderTradeCenterResponse apply(OrderTradeCenterRequest request)
             throws BusinessException, SystemException {
@@ -39,18 +31,9 @@ public class OrderTradeCenterSVImpl implements IOrderTradeCenterSV {
     	boolean ccsMqFlag=false;
     	//从配置中心获取ccsMqFlag
     	ccsMqFlag=MQConfigUtil.getCCSMqFlag();
-    	
     	//非消息模式下，同步调用服务
     	if(!ccsMqFlag){
     		OrderTradeCenterResponse response = ordOrderTradeBusiSV.apply(request);
-    		//刷新搜索引擎数据
-    		List<OrderResInfo> orderResInfos = response.getOrderResInfos();
-    		for (OrderResInfo orderResInfo : orderResInfos) {
-    			SesDataRequest sesReq=new SesDataRequest();
-    			sesReq.setTenantId(request.getTenantId());
-    			sesReq.setParentOrderId(orderResInfo.getOrderId());
-    			this.orderIndexBusiSV.insertSesData(sesReq);
-			}
     		ResponseHeader responseHeader = new ResponseHeader(true,
     				ExceptCodeConstants.Special.SUCCESS, "成功");
     		response.setResponseHeader(responseHeader);
