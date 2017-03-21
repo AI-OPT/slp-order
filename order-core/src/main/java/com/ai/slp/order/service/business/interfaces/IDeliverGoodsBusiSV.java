6ad7cda18dc25e0bc3ca661348008466beba4daa
fazhicao <409993207@qq.com>
2017-03-21 15:17:02 +0800

    事务拆分

--- order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IDeliverGoodsBusiSV.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IDeliverGoodsBusiSV.java
@@ -3,9 +3,10 @@ package com.ai.slp.order.service.business.interfaces;
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
 import com.ai.slp.order.api.delivergoods.param.DeliverGoodsRequest;
+import com.ai.slp.order.dao.mapper.bo.OrdOrder;
 
 public interface IDeliverGoodsBusiSV {
 	//订单发货
-	public void deliverGoods(DeliverGoodsRequest request) throws BusinessException, SystemException;
+	public void deliverGoods(DeliverGoodsRequest request,OrdOrder ordOrder) throws BusinessException, SystemException;
 
 }
