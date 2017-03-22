e6c882cfca1e05b3e970d1ab2f59fa4b27afaac1
fazhicao <409993207@qq.com>
2017-03-22 17:23:16 +0800

    订单轨迹封装公共方法

---
+++ order-core/src/main/java/com/ai/slp/order/util/OrderStateChgUtil.java
@@ -0,0 +1,29 @@
+package com.ai.slp.order.util;
+
+import java.sql.Timestamp;
+
+import com.ai.slp.order.constants.OrdersConstants.OrdOdStateChg;
+import com.ai.slp.order.vo.OrderStateChgVo;
+
+/**
+ * 订单轨迹参数封装
+ * @date 2017年3月22日 
+ * @author caofz
+ */
+public class OrderStateChgUtil {
+	 public static OrderStateChgVo getOrderStateChg(Long orderId, String tenantId, String orgState, String newState,
+	            String chgDesc, String orgId, String operId, String operName, Timestamp timestamp){
+		   OrderStateChgVo stateChgVo=new OrderStateChgVo();
+           stateChgVo.setOrderId(orderId);
+           stateChgVo.setTenantId(tenantId);
+           stateChgVo.setOrgState(orgState);
+           stateChgVo.setNewState(newState);
+           stateChgVo.setChgDesc( OrdOdStateChg.ChgDesc.ORDER_PAID);
+           stateChgVo.setOrgId(orgId);
+           stateChgVo.setOperId(operId);
+           stateChgVo.setOperName(operName);
+           stateChgVo.setTimestamp(timestamp);
+	       return stateChgVo;
+	    }
+
+}
