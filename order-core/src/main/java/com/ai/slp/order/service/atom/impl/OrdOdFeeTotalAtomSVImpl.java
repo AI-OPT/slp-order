c13be5ca8ef2a16633b5c0bbbf7fa9c2e49c0e9c
fazhicao <409993207@qq.com>
2017-04-11 17:00:50 +0800

    优化代码

--- order-core/src/main/java/com/ai/slp/order/service/atom/impl/OrdOdFeeTotalAtomSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/service/atom/impl/OrdOdFeeTotalAtomSVImpl.java
@@ -2,48 +2,52 @@ package com.ai.slp.order.service.atom.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.ai.slp.order.dao.mapper.bo.OrdOdFeeTotal;
 import com.ai.slp.order.dao.mapper.bo.OrdOdFeeTotalCriteria;
-import com.ai.slp.order.dao.mapper.factory.MapperFactory;
+import com.ai.slp.order.dao.mapper.interfaces.OrdOdFeeTotalMapper;
 import com.ai.slp.order.service.atom.interfaces.IOrdOdFeeTotalAtomSV;
 
 @Component
 public class OrdOdFeeTotalAtomSVImpl implements IOrdOdFeeTotalAtomSV {
+	
+	@Autowired
+	private OrdOdFeeTotalMapper ordOdFeeTotalMapper;
 
     @Override
     public List<OrdOdFeeTotal> selectByExample(OrdOdFeeTotalCriteria example) {
-        return MapperFactory.getOrdOdFeeTotalMapper().selectByExample(example);
+        return ordOdFeeTotalMapper.selectByExample(example);
     }
 
     @Override
     public OrdOdFeeTotal selectByOrderId(String tenantId, long orderId) {
         OrdOdFeeTotalCriteria example = new OrdOdFeeTotalCriteria();
         example.createCriteria().andTenantIdEqualTo(tenantId).andOrderIdEqualTo(orderId);
-        List<OrdOdFeeTotal> ordOdFeeTotals = MapperFactory.getOrdOdFeeTotalMapper()
+        List<OrdOdFeeTotal> ordOdFeeTotals = ordOdFeeTotalMapper
                 .selectByExample(example);
         return ordOdFeeTotals == null || ordOdFeeTotals.isEmpty() ? null : ordOdFeeTotals.get(0);
     }
 
     @Override
     public int insertSelective(OrdOdFeeTotal record) {
-        return MapperFactory.getOrdOdFeeTotalMapper().insertSelective(record);
+        return ordOdFeeTotalMapper.insertSelective(record);
     }
 
     @Override
     public int updateByOrderId(OrdOdFeeTotal ordOdFeeTotal) {
-        return MapperFactory.getOrdOdFeeTotalMapper().updateByPrimaryKey(ordOdFeeTotal);
+        return ordOdFeeTotalMapper.updateByPrimaryKey(ordOdFeeTotal);
     }
 
 	@Override
 	public int updateByExampleSelective(OrdOdFeeTotal record, OrdOdFeeTotalCriteria example) {
-		return MapperFactory.getOrdOdFeeTotalMapper().updateByExampleSelective(record, example);
+		return ordOdFeeTotalMapper.updateByExampleSelective(record, example);
 	}
 
 	@Override
 	public int countByExample(OrdOdFeeTotalCriteria example) {
-		return MapperFactory.getOrdOdFeeTotalMapper().countByExample(example);
+		return ordOdFeeTotalMapper.countByExample(example);
 	}
 
 }
