617989161996faeb1564fc70fc5788a9d4939436
fazhicao <409993207@qq.com>
2017-04-11 19:19:36 +0800

    刷新搜索引擎状态数据

--- order-core/src/main/java/com/ai/slp/order/service/business/interfaces/search/IOrderIndexBusiSV.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/interfaces/search/IOrderIndexBusiSV.java
@@ -3,10 +3,13 @@ package com.ai.slp.order.service.business.interfaces.search;
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
 import com.ai.slp.order.api.sesdata.param.SesDataRequest;
+import com.ai.slp.order.dao.mapper.bo.OrdOrder;
 
 public interface IOrderIndexBusiSV {
 	 //新增订单信息
 	 public boolean insertSesData(SesDataRequest request) throws BusinessException, SystemException; 
+	 //刷新搜索引擎状态数据
+	 public void refreshStateData(OrdOrder ordOrder) throws BusinessException, SystemException; 
 	 
 
 }
