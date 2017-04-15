f276f00da2c868ddc3e16c50c32696ebdefcfaa3
fazhicao <409993207@qq.com>
2017-04-15 12:07:39 +0800

    添加刷新搜索引擎实现

--- order-core/src/main/java/com/ai/slp/order/service/business/impl/search/OrderIndexBusiSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/impl/search/OrderIndexBusiSVImpl.java
@@ -3,6 +3,8 @@ package com.ai.slp.order.service.business.impl.search;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,7 +18,9 @@ import com.ai.paas.ipaas.search.vo.Result;
 import com.ai.paas.ipaas.search.vo.SearchCriteria;
 import com.ai.platform.common.api.cache.interfaces.ICacheSV;
 import com.ai.platform.common.api.cache.param.SysParam;
+import com.ai.slp.order.api.sesdata.param.SesDataByPageRequest;
 import com.ai.slp.order.api.sesdata.param.SesDataRequest;
+import com.ai.slp.order.api.sesdata.param.SesDataResponse;
 import com.ai.slp.order.constants.OrdersConstants;
 import com.ai.slp.order.constants.SearchConstants;
 import com.ai.slp.order.dao.mapper.bo.OrdOdFeeProd;
@@ -40,6 +44,8 @@ import com.ai.slp.order.util.InfoTranslateUtil;
 
 @Service
 public class OrderIndexBusiSVImpl implements IOrderIndexBusiSV {
+	
+	private static Logger logger=LoggerFactory.getLogger(OrderIndexBusiSVImpl.class);
 
 	@Autowired
 	private IOrdOdProdAtomSV ordOdProdAtomSV;
@@ -218,5 +224,112 @@ public class OrderIndexBusiSVImpl implements IOrderIndexBusiSV {
 			}
 		}
 		SESClientFactory.getSearchClient(SearchConstants.SearchNameSpace).bulkInsert(ordList);
+	}
+
+	
+	@Override
+	public SesDataResponse insertSesDataByPage(SesDataByPageRequest request) throws BusinessException, SystemException {
+		long orderId=0;
+		int startSize = 1;
+		int maxSize = 1;
+		// 最大条数设置
+		int pageNo = request.getPageNo();
+		int size = request.getPageSize();
+		if (pageNo < 1) {
+			startSize = 0;
+		} else {
+			startSize = (pageNo - 1) * size;
+		}
+		maxSize = size;
+		SesDataResponse response=new SesDataResponse();
+		ICacheSV iCacheSV = DubboConsumerFactory.getService(ICacheSV.class);
+	 	String tenantId = request.getTenantId();
+	 	//性能数据
+	 	int queryCount = ordOrderAtomSV.countForSes();
+	 	int failCount=0;
+	 	int shareParentCount=0;
+	 	List<OrdOrder> ordOrderDatas = ordOrderAtomSV.selectSesData(startSize,maxSize);
+		List<OrderInfo> orderList = new ArrayList<OrderInfo>();
+		if(!CollectionUtil.isEmpty(ordOrderDatas)){
+		for (OrdOrder order: ordOrderDatas) {
+			OrdOrder ord = ordOrderAtomSV.selectByOrderId(tenantId, order.getParentOrderId());
+			if(ord==null) {
+				failCount++;
+				logger.error(">>>>>>>>>>不存在订单主表信息! 父订单id:"+ order.getParentOrderId());
+				continue;
+			}
+			//父订单
+			if(orderId==ord.getOrderId()) {
+				shareParentCount++;
+			}
+			orderId = ord.getOrderId();
+			OrderInfo ordInfo = new OrderInfo();
+			ordInfo.setTenantid(tenantId);
+			ordInfo.setChlid(ord.getChlId());
+			//翻译渠道来源
+			SysParam sysParamChlId = InfoTranslateUtil.translateInfo(tenantId,
+					"ORD_ORDER", "CHL_ID", ord.getChlId(), iCacheSV);
+			ordInfo.setChlidname(sysParamChlId == null ? "" : sysParamChlId.getColumnDesc());
+			ordInfo.setPorderid(orderId);
+			ordInfo.setUsername(ord.getUserName());
+			ordInfo.setUsertel(ord.getUserTel());
+			ordInfo.setDeliveryflag(ord.getDeliveryFlag());
+			ordInfo.setFlag(ord.getFlag());
+			//翻译是否需要物流
+			SysParam sysParamDf = InfoTranslateUtil.translateInfo(tenantId, "ORD_ORDER",
+					"ORD_DELIVERY_FLAG", ord.getDeliveryFlag(), iCacheSV);
+			ordInfo.setDeliveryflagname(sysParamDf == null ? "" : sysParamDf.getColumnDesc());
+			ordInfo.setOrdertime(ord.getOrderTime());
+			ordInfo.setParentorderstate(ord.getState());
+			ordInfo.setSupplierid(ord.getSupplierId());
+			// 获取手机号
+			OrdOdLogistics ordOdLogistics = ordOdLogisticsAtomSV.selectByOrd(tenantId, orderId);
+			if(ordOdLogistics==null) {
+				failCount++;
+				logger.error(">>>>>>>>>>不存在订单运费信息! 父订单id:"+ orderId);
+				continue;
+			}
+			ordInfo.setContacttel(ordOdLogistics.getContactTel());
+			// 获取积分
+			List<OrdOdFeeProd> orderFeeProdList = ordOdFeeProdAtomSV.selectByOrderId(orderId);
+			if(CollectionUtil.isEmpty(orderFeeProdList)) {
+				failCount++;
+				logger.error(">>>>>>>>>>不存在订单费用明细信息! 父订单id:"+ orderId);
+				continue;
+			}
+			//存在的情况下
+			long points = 0; 
+			for (OrdOdFeeProd ordOdFeeProd : orderFeeProdList) {
+				if (OrdersConstants.OrdOdFeeProd.PayStyle.JF.equals(ordOdFeeProd.getPayStyle())) {
+					points += ordOdFeeProd.getPaidFee();
+					break;
+				}
+			}
+			ordInfo.setPoints(points);
+			// 查询费用信息
+			OrdOdFeeTotal ordOdFeeTotal = ordOdFeeTotalAtomSV.selectByOrderId(tenantId, orderId);
+			if (ordOdFeeTotal == null) {
+				failCount++;
+				logger.error(">>>>>>>>>>不存在订单费用主表信息! 父订单id:"+ orderId);
+				continue;
+			}
+			//存在情况下
+			ordInfo.setDiscountfee(ordOdFeeTotal.getDiscountFee());
+			ordInfo.setAdjustfee(ordOdFeeTotal.getAdjustFee());
+			// 查询订单其它信息
+			ordInfo = this.queryOrdProdExtends(ordInfo, ord, 
+					iCacheSV, orderId);
+			orderList.add(ordInfo);
+		}
+		try{
+			SESClientFactory.getSearchClient(SearchConstants.SearchNameSpace).bulkInsert(orderList);
+		}catch(Exception e){
+			throw new SystemException("","订单信息加入搜索引擎失败,订单ID:"+orderId);
+		}
+		}
+		response.setFailCount(failCount);
+		response.setQueryCount(queryCount);
+		response.setShareParentCount(shareParentCount);
+		return response;
 	}
 }
