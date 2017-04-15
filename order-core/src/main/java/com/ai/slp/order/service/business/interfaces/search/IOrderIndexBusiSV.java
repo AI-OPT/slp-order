91fee83ecda78e81b9f837421552a2fa7bbed3dd
fazhicao <409993207@qq.com>
2017-04-15 12:07:46 +0800

    添加刷新搜索引擎接口

--- order-core/src/main/java/com/ai/slp/order/service/business/interfaces/search/IOrderIndexBusiSV.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/interfaces/search/IOrderIndexBusiSV.java
@@ -2,7 +2,9 @@ package com.ai.slp.order.service.business.interfaces.search;
 
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
+import com.ai.slp.order.api.sesdata.param.SesDataByPageRequest;
 import com.ai.slp.order.api.sesdata.param.SesDataRequest;
+import com.ai.slp.order.api.sesdata.param.SesDataResponse;
 import com.ai.slp.order.dao.mapper.bo.OrdOrder;
 
 public interface IOrderIndexBusiSV {
@@ -10,6 +12,6 @@ public interface IOrderIndexBusiSV {
 	 public boolean insertSesData(SesDataRequest request) throws BusinessException, SystemException; 
 	 //刷新搜索引擎状态数据
 	 public void refreshStateData(OrdOrder ordOrder,OrdOrder pOrder) throws BusinessException, SystemException; 
-	 
-
+	 //分页刷新数据
+	 public SesDataResponse insertSesDataByPage(SesDataByPageRequest request) throws BusinessException, SystemException; 
 }
