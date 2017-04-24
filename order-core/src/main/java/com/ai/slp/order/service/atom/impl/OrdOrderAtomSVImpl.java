d4cae930a5d7c76daddbe39fc1af226d32d3ff3b
fazhicao <409993207@qq.com>
2017-04-24 17:47:49 +0800

    优化

--- order-core/src/main/java/com/ai/slp/order/service/atom/impl/OrdOrderAtomSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/service/atom/impl/OrdOrderAtomSVImpl.java
@@ -1,7 +1,6 @@
 package com.ai.slp.order.service.atom.impl;
 
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
