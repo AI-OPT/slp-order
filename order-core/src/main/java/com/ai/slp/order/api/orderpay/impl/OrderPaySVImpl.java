6d48e213636db1e55b13e7c4ab36934034b33d1a
fazhicao <409993207@qq.com>
2017-04-26 11:40:09 +0800

    冗余代码去掉

--- order-core/src/main/java/com/ai/slp/order/api/orderpay/impl/OrderPaySVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/api/orderpay/impl/OrderPaySVImpl.java
@@ -9,18 +9,15 @@ import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
 import com.ai.opt.base.vo.BaseResponse;
 import com.ai.opt.base.vo.ResponseHeader;
-import com.ai.opt.sdk.components.mds.MDSClientFactory;
 import com.ai.opt.sdk.constants.ExceptCodeConstants;
 import com.ai.slp.order.api.orderpay.interfaces.IOrderPaySV;
 import com.ai.slp.order.api.orderpay.param.OrderOidRequest;
 import com.ai.slp.order.api.orderpay.param.OrderPayRequest;
-import com.ai.slp.order.constants.OrdersConstants;
 import com.ai.slp.order.dao.mapper.bo.OrdOrder;
 import com.ai.slp.order.service.atom.interfaces.IOrdOrderAtomSV;
 import com.ai.slp.order.service.business.interfaces.IOrderPayBusiSV;
 import com.ai.slp.order.util.ValidateUtils;
 import com.alibaba.dubbo.config.annotation.Service;
-import com.alibaba.fastjson.JSON;
 
 /**
  * 订单收费服务 Date: 2016年5月24日 <br>
@@ -59,31 +56,17 @@ public class OrderPaySVImpl implements IOrderPaySV {
 	public BaseResponse returnOid(OrderOidRequest request) throws BusinessException, SystemException {
 		/* 参数校验*/
 		ValidateUtils.validateReturnOid(request);
-		boolean ccsMqFlag=false;
-		//ccsMqFlag = MQConfigUtil.getCCSMqFlag();
-		//非消息模式 同步调用服务
-		if(!ccsMqFlag) {
-			BaseResponse response = new BaseResponse();
-			OrdOrder order = ordOrderAtomSV.selectByPrimaryKey(request.getOrderId());
-			if(order==null) {
-				throw new BusinessException("", "订单信息不存在[订单id:"+request.getOrderId()+
-						",租户id:"+request.getTenantId()+"]");
-			}
-			order.setDownstreamOrderId(request.getOid());
-	        orderPayBusiSV.returnOid(order);
-	        ResponseHeader responseHeader = new ResponseHeader(true,
-	                ExceptCodeConstants.Special.SUCCESS, "成功");
-	        response.setResponseHeader(responseHeader);
-		    return response;  
-		}else {
-			//消息模式下 异步调用服务
-			 BaseResponse response = new BaseResponse();
-			 MDSClientFactory.getSenderClient(OrdersConstants.MDSNS.MDS_NS_ORDER_RETURNOID_TOPIC).send(JSON.toJSONString(request), 0);
-	         ResponseHeader responseHeader = new ResponseHeader(true,
-	                ExceptCodeConstants.Special.SUCCESS, "成功");
-	         response.setResponseHeader(responseHeader);
-		     return response;  
+		BaseResponse response = new BaseResponse();
+		OrdOrder order = ordOrderAtomSV.selectByPrimaryKey(request.getOrderId());
+		if(order==null) {
+			throw new BusinessException("", "订单信息不存在[订单id:"+request.getOrderId()+
+					",租户id:"+request.getTenantId()+"]");
 		}
-		
+		order.setDownstreamOrderId(request.getOid());
+        orderPayBusiSV.returnOid(order);
+        ResponseHeader responseHeader = new ResponseHeader(true,
+                ExceptCodeConstants.Special.SUCCESS, "成功");
+        response.setResponseHeader(responseHeader);
+	    return response;  
 	}
 }
