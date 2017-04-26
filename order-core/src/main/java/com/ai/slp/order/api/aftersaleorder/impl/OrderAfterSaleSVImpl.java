b1ba675c9a93007b337bee7ec9847a339397e629
fazhicao <409993207@qq.com>
2017-04-26 11:39:47 +0800

    1

--- order-core/src/main/java/com/ai/slp/order/api/aftersaleorder/impl/OrderAfterSaleSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/api/aftersaleorder/impl/OrderAfterSaleSVImpl.java
@@ -1,13 +1,10 @@
 package com.ai.slp.order.api.aftersaleorder.impl;
 
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
