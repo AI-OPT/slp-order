8122d458aad2eb8c69e8035b264ce2dbfb1fa147
fazhicao <409993207@qq.com>
2017-04-26 11:46:03 +0800

    订单详情修改

--- order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IOrdOrderBusiSV.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/interfaces/IOrdOrderBusiSV.java
@@ -2,16 +2,15 @@ package com.ai.slp.order.service.business.interfaces;
 
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
-import com.ai.platform.common.api.cache.interfaces.ICacheSV;
 import com.ai.slp.order.api.orderlist.param.BehindQueryOrderListRequest;
 import com.ai.slp.order.api.orderlist.param.BehindQueryOrderListResponse;
+import com.ai.slp.order.api.orderlist.param.QueryOrderRequest;
 import com.ai.slp.order.api.orderlist.param.QueryOrderResponse;
-import com.ai.slp.order.dao.mapper.bo.OrdOdFeeTotal;
 import com.ai.slp.order.dao.mapper.bo.OrdOrder;
 
 public interface IOrdOrderBusiSV {
 
-    public QueryOrderResponse queryOrder(OrdOdFeeTotal ordOdFeeTotal, OrdOrder order,ICacheSV iCacheSV) 
+    public QueryOrderResponse queryOrder(QueryOrderRequest orderRequest) 
     		throws BusinessException,SystemException;
 
     public BehindQueryOrderListResponse behindQueryOrderList(BehindQueryOrderListRequest orderListRequest)
