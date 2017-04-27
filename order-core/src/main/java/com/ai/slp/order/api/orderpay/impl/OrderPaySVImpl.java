9217edae5932d3a1c2067d1efbfd5920845e0dc2
fazhicao <409993207@qq.com>
2017-04-27 10:43:57 +0800

    优化

--- order-core/src/main/java/com/ai/slp/order/api/orderpay/impl/OrderPaySVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/api/orderpay/impl/OrderPaySVImpl.java
@@ -14,7 +14,6 @@ import com.ai.slp.order.api.orderpay.interfaces.IOrderPaySV;
 import com.ai.slp.order.api.orderpay.param.OrderOidRequest;
 import com.ai.slp.order.api.orderpay.param.OrderPayRequest;
 import com.ai.slp.order.dao.mapper.bo.OrdOrder;
-import com.ai.slp.order.service.atom.interfaces.IOrdOrderAtomSV;
 import com.ai.slp.order.service.business.interfaces.IOrderPayBusiSV;
 import com.ai.slp.order.util.ValidateUtils;
 import com.alibaba.dubbo.config.annotation.Service;
@@ -33,9 +32,6 @@ public class OrderPaySVImpl implements IOrderPaySV {
     
     @Autowired
     private IOrderPayBusiSV orderPayBusiSV;
-    @Autowired
-    private IOrdOrderAtomSV ordOrderAtomSV;
-    
     /**
      * 订单收费
      */
@@ -57,11 +53,8 @@ public class OrderPaySVImpl implements IOrderPaySV {
 		/* 参数校验*/
 		ValidateUtils.validateReturnOid(request);
 		BaseResponse response = new BaseResponse();
-		OrdOrder order = ordOrderAtomSV.selectByPrimaryKey(request.getOrderId());
-		if(order==null) {
-			throw new BusinessException("", "订单信息不存在[订单id:"+request.getOrderId()+
-					",租户id:"+request.getTenantId()+"]");
-		}
+		OrdOrder order =new OrdOrder();
+		order.setOrderId(request.getOrderId());
 		order.setDownstreamOrderId(request.getOid());
         orderPayBusiSV.returnOid(order);
         ResponseHeader responseHeader = new ResponseHeader(true,
