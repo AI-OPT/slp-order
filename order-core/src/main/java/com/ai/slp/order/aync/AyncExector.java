96e98e157e91d1b98b6dfe2a2073a6b54c0ed684
fazhicao <409993207@qq.com>
2017-05-03 10:52:39 +0800

    添加注释

--- order-core/src/main/java/com/ai/slp/order/aync/AyncExector.java
+++ order-core/src/main/java/com/ai/slp/order/aync/AyncExector.java
@@ -2,7 +2,11 @@ package com.ai.slp.order.aync;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
+/**
+ * 线程池操作
+ * @date 2017年5月3日 
+ * @author caofz
+ */
 public class AyncExector {
 	private static ExecutorService pool = Executors.newFixedThreadPool(10);
 	
