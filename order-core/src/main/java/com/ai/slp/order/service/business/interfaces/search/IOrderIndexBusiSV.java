73dfb742f1272989bff84ff205f3f5108d8b831d
fazhicao <409993207@qq.com>
2017-05-03 09:47:10 +0800

    删除es数据

--- order-core/src/main/java/com/ai/slp/order/service/business/interfaces/search/IOrderIndexBusiSV.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/interfaces/search/IOrderIndexBusiSV.java
@@ -2,6 +2,7 @@ package com.ai.slp.order.service.business.interfaces.search;
 
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
+import com.ai.slp.order.api.orderlist.param.BehindQueryOrderListRequest;
 import com.ai.slp.order.api.sesdata.param.SesDataByPageRequest;
 import com.ai.slp.order.api.sesdata.param.SesDataRequest;
 import com.ai.slp.order.api.sesdata.param.SesDataResponse;
@@ -14,4 +15,7 @@ public interface IOrderIndexBusiSV {
 	 public void refreshStateData(OrdOrder ordOrder,OrdOrder pOrder) throws BusinessException, SystemException; 
 	 //分页刷新数据
 	 public SesDataResponse insertSesDataByPage(SesDataByPageRequest request) throws BusinessException, SystemException; 
+
+	 //删除es数据
+	 public void deleteSesData(BehindQueryOrderListRequest request) throws BusinessException, SystemException;
 }
