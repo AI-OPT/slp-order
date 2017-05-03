177cd3b5375aba84a98784bc0552445f40723377
fazhicao <409993207@qq.com>
2017-05-03 10:53:57 +0800

    添加注释

--- order-core/src/main/java/com/ai/slp/order/service/business/interfaces/search/IOrderSearch.java
+++ order-core/src/main/java/com/ai/slp/order/service/business/interfaces/search/IOrderSearch.java
@@ -11,9 +11,40 @@ import com.ai.slp.order.search.bo.OrderInfo;
 import com.ai.slp.order.search.dto.OrderSearchCriteria;
 
 public interface IOrderSearch {
+	/**
+	 * 按条件搜索ses数据
+	 * @param criteria
+	 * @return
+	 * @author caofz
+	 * @ApiDocMethod
+	 * @ApiCode 
+	 * @RestRelativeURL
+	 */
     Result<Map<String, Object>> search(OrderSearchCriteria criteria);
+    
+    /**
+     * 按照条件,排序,页数查询es数据
+     * @param searchCriterias
+     * @param from
+     * @param offset
+     * @param sorts
+     * @return
+     * @author caofz
+     * @ApiDocMethod
+     * @ApiCode 
+     * @RestRelativeURL
+     */
     Result<OrderInfo> search(List<SearchCriteria>searchCriterias, int from,int offset, List<Sort> sorts);
     
+    /**
+     * 按条件查询个数
+     * @param searchCriterias
+     * @return
+     * @author caofz
+     * @ApiDocMethod
+     * @ApiCode 
+     * @RestRelativeURL
+     */
     int countAll(List<SearchCriteria> searchCriterias);
 
 }
