3444ffd99c34aad193c2d20d2eb3f7d344e19902
fazhicao <409993207@qq.com>
2017-05-03 10:52:14 +0800

    冗余代码清除

--- order-core/src/main/java/com/ai/slp/order/api/aftersaleorder/impl/OrderAfterSaleSVImpl.java
+++ order-core/src/main/java/com/ai/slp/order/api/aftersaleorder/impl/OrderAfterSaleSVImpl.java
@@ -1,8 +1,6 @@
 package com.ai.slp.order.api.aftersaleorder.impl;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -26,7 +24,6 @@ import com.alibaba.dubbo.config.annotation.Service;
 @Component
 public class OrderAfterSaleSVImpl implements IOrderAfterSaleSV {
 	
-	private static final Logger logger=LoggerFactory.getLogger(OrderAfterSaleSVImpl.class);
 	@Autowired
 	private IOrderAfterSaleBusiSV orderAfterSaleBusiSV;
 	@Autowired
